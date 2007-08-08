5ba8dfd73325b288233ccad634d4dcf6f6e548f5
Yaron Koren <yaron@users.mediawiki.org>
2007-08-08 17:48:39 +0000

    Updated: input for the 'semantic property' is now a dropdown, not a text field; template format is settable; link to 'create property' form, other related changes

--- specials/SF_CreateTemplate.php
+++ specials/SF_CreateTemplate.php
@@ -15,30 +15,58 @@ require_once( "$IP/includes/SpecialPage.php" );
 
 SpecialPage::addPage( new SpecialPage('CreateTemplate','',true,'doSpecialCreateTemplate',false) );
 
-// beginning of new layout for CreateTemplate
+function cmp($a, $b)
+{
+    if ($a == $b) {
+        return 0;
+    } elseif ($a < $b) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
 function getSemanticProperties() {
-	$dbr =& wfGetDB( DB_SLAVE );
-	$attributes = $dbr->tableName( 'smw_attributes' );
-	// QueryPage uses the value from this SQL in an ORDER clause,
-	// so return attribute title in value, and its type in title.
-	$query = "SELECT distinct value_datatype as title,
-		  attribute_title as value
-		  FROM $attributes
-		  GROUP BY attribute_title, value_datatype";
+  $dbr =& wfGetDB( DB_SLAVE );
+  $all_properties = array();
+
+  $res = $dbr->query("SELECT distinct attribute_title FROM " .
+    $dbr->tableName('smw_attributes'));
+  while ($row = $dbr->fetchRow($res)) {
+    $attribute_name = str_replace('_', ' ', $row[0]);
+    $all_properties[$attribute_name . ":="] = $attribute_name;
+  }
+  $dbr->freeResult($res);
+
+  $res = $dbr->query("SELECT distinct relation_title FROM " .
+    $dbr->tableName('smw_relations'));
+  while ($row = $dbr->fetchRow($res)) {
+    $relation_name = str_replace('_', ' ', $row[0]);
+    $all_properties[$relation_name . "::"] = $relation_name;
+  }
+  $dbr->freeResult($res);
+
+  uasort($all_properties, "cmp");
+  return $all_properties;
 }
 
-function printFieldEntryBox($id, $f) {
+function printPropertiesDropdown($all_properties, $id, $property) {
+  $dropdown_str = "<select name=\"semantic_field_call_$id\">\n";
+  $dropdown_str .= "<option value=\"\"></option>\n";
+  foreach ($all_properties as $prop_id => $prop_name) {
+    $selected = ($property == $prop_id) ? "selected" : "";
+    $dropdown_str .= "<option value=\"$prop_id\" $selected>$prop_name</option>\n";
+  }
+  $dropdown_str .= "</select>\n";
+  return $dropdown_str;
+}
+
+function printFieldEntryBox($id, $f, $all_properties) {
+  $dropdown_html = printPropertiesDropdown($all_properties, $id, $f->semantic_field_call);
   $text = '	<div class="field_box">' . "\n";
   $text .= '	<p>' . wfMsg('sf_createtemplate_fieldname') . ' <input size="15" name="name_' . $id . '" value="' . $f->field_name . '">' . "\n";
-  $text .= '	' . wfMsg('sf_createtemplate_displaylabel') . ' <input size="15" name="label_' . $id . '" value="' . $f->label . '"></p>' . "\n";
-  $text .= '	<p>' . wfMsg('sf_createtemplate_semanticproperty') . ' <input size="15" name="semantic_field_' . $id . '" value="' . $f->semantic_field . '">' . "\n";
+  $text .= '	' . wfMsg('sf_createtemplate_displaylabel') . ' <input size="15" name="label_' . $id . '" value="' . $f->label . '">' . "\n";
+  $text .= '	' . wfMsg('sf_createtemplate_semanticproperty') . ' ' . $dropdown_html . $f->semantic_field . "\n";
 
-  $text .= "	<input type=\"radio\" name=\"attr_or_rel_$id\" value=\"attribute\"" .
-    ($f->attr_or_rel == "attribute" ? " checked" : "") . '> ' .
-    wfMsg('sf_createtemplate_attribute') . "\n";
-  $text .= "	<input type=\"radio\" name=\"attr_or_rel_$id\" value=\"relation\"" .
-    ($f->attr_or_rel == "relation" ? " checked" : "") . '> ' .
-    wfMsg('sf_createtemplate_relation') . "\n";
   $text .= "&nbsp;&nbsp;\n\n";
 
   if ($id != "new") {
@@ -53,10 +81,13 @@ END;
 }
 
 function doSpecialCreateTemplate() {
-  global $wgOut, $wgRequest;
+  global $wgOut, $wgRequest, $wgUser;
+
+  $all_properties = getSemanticProperties();
+
   # cycle through the query values, setting the appropriate local variables
   $template_name = $wgRequest->getVal('template_name');
-  $type_name = $wgRequest->getVal('type_name');
+  $category = $wgRequest->getVal('category');
   $cur_id = 1;
   $fields = array();
   foreach ($wgRequest->getValues() as $var => $val) {
@@ -67,14 +98,14 @@ function doSpecialCreateTemplate() {
           # do nothing - this field won't get added to the new list
         } else {
           $field = SFTemplateField::newWithValues($val,
-            $wgRequest->getVal('label_' . $old_id),
-            $wgRequest->getVal('semantic_field_' . $old_id),
-            $wgRequest->getVal('attr_or_rel_' . $old_id));
+            $wgRequest->getVal('label_' . $old_id), null, null);
+          $field->semantic_field_call = $wgRequest->getVal('semantic_field_call_' . $old_id);
           $fields[] = $field;
         }
       }
     }
   }
+  $template_format = $wgRequest->getVal('template_format');
 
   $preview_button_text = wfMsg('preview');
   if ($wgRequest->getVal('preview') == $preview_button_text) {
@@ -85,7 +116,9 @@ function doSpecialCreateTemplate() {
       # redirect to wiki interface
       $title = Title::newFromText($template_name, NS_TEMPLATE);
       $submit_url = $title->getLocalURL('action=submit');
-      $full_text = create_template_text($template_name, $fields, $type_name);
+      $full_text = createTemplateText($template_name, $fields, $category, $template_format);
+      // HTML-encode
+      $full_text = str_replace('"', '&quot;', $full_text);
       $text .= <<<END
   <form id="editform" name="editform" method="post" action="$submit_url">
     <input type="hidden" name="wpTextbox1" id="wpTextbox1" value="$full_text" />
@@ -102,21 +135,29 @@ END;
   // set 'title' field, in case there's no URL niceness
   $text .= '    <input type="hidden" name="title" value="Special:CreateTemplate">' . "\n";
   $text .= '	<p>' . wfMsg('sf_createtemplate_namelabel') . ' <input size="25" name="template_name" value="' . $template_name . '"> <font color="red">' . $template_name_error_str . '</font></p>' . "\n";
-  $text .= '	<p>' . wfMsg('sf_createtemplate_categorylabel') . ' <input size="25" name="type_name" value="' . $type_name . '"></p>' . "\n";
+  $text .= '	<p>' . wfMsg('sf_createtemplate_categorylabel') . ' <input size="25" name="category" value="' . $category . '"></p>' . "\n";
   $text .= "	<fieldset>\n";
   $text .= '	<legend>' . wfMsg('sf_createtemplate_templatefields') . "</legend>\n";
   $text .= '	<p>' . wfMsg('sf_createtemplate_fieldsdesc') . "</p>\n";
 
   foreach ($fields as $i => $field) {
-    $text .= printFieldEntryBox($i + 1, $field);
+    $text .= printFieldEntryBox($i + 1, $field, $all_properties);
   }
   $new_field = new SFTemplateField();
-  $text .= printFieldEntryBox("new", $new_field);
+  $text .= printFieldEntryBox("new", $new_field, $all_properties);
 
   $text .= '	<p><input type="submit" value="' . wfMsg('sf_createtemplate_addfield') . '"></p>' . "\n";
   $text .= "	</fieldset>\n";
+  $text .= '	<p>' . wfMsg('sf_createtemplate_outputformat') . "\n";
+  $text .= '	<input type="radio" name="template_format" checked value="standard">' . wfMsg('sf_createtemplate_standardformat') . "\n";
+  $text .= '	<input type="radio" name="template_format" value="infobox">' . wfMsg('sf_createtemplate_infoboxformat') . "</p>\n";
   $text .= '	<p><input type="submit" name="preview" value="' . wfMsg('preview') . '"></p>' . "\n";
   $text .= "	</form>\n";
+  $sk = $wgUser->getSkin();
+  $cp = SpecialPage::getPage('CreateProperty');
+  $create_property_link = $sk->makeKnownLinkObj($cp->getTitle(), $cp->getDescription());
+  $text .= "	<br /><hr /><br />\n";
+  $text .= "	<p>$create_property_link.</p>\n";
 
   $wgOut->addLink( array(
     'rel' => 'stylesheet',
