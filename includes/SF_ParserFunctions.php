2f0dce3400d9aa16dfd23f8ab1f58d2d2c51e2b6
Yaron Koren <yaron@users.mediawiki.org>
2007-08-21 18:34:53 +0000

    New file - contains one parser function, #sf_forminput

---
+++ includes/SF_ParserFunctions.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Parser functions for Semantic Forms.
+ * Currently only one pareser function is defined: sf_forminput. It is
+ * called as:
+ *
+ * {{#sf_forminput|form_name|size|value|button_text|query_string}}
+ *
+ * This functions returns HTML representing a form to let the user enter the
+ * name of a page to be added or edited using a Semantic Forms form. All
+ * arguments are optional. form_name is the name of the SF form to be used;
+ * if it is left empty, a dropdown will appear, letting the user chose among
+ * all existing forms. size represents the size of the text input (default
+ * is 25), and value is the starting value of the input (default is blank).
+ * button_text is the text that will appear on the "submit" button, and
+ * query_string is the set of values that you want passed in through the
+ * query string to the form.
+ *
+ * Example: to create an input to add or edit a page with a form called
+ * 'User' within a namespace also called 'User', and to have the form
+ * preload with the page called 'UserStub', you could call the following:
+ *
+ * {{#sf_forminput|User|||Add or edit user|namespace=User&preload=UserStub}}
+ *
+ * @author Yaron Koren
+ */
+
+
+function sfgParserFunctions () {
+    global $wgParser;
+    $wgParser->setFunctionHook('sf_forminput', 'renderFormInput');
+}
+
+function sfgLanguageGetMagic( &$magicWords, $langCode = "en" ) {
+	switch ( $langCode ) {
+	default:
+		$magicWords['sf_forminput']	= array ( 0, 'sf_forminput' );
+	}
+	return true;
+}
+
+function renderFormInput (&$parser, $inFormName = '', $inSize = '25', $inValue = '', $inButtonStr = '', $inQueryStr = '') {
+	$ap = SpecialPage::getPage('AddPage');
+	$ap_url = $ap->getTitle()->getLocalURL();
+	$str = <<<END
+			<form action="$ap_url" method="get">
+			<p><input type="text" name="page_name" size="$inSize" value="$inValue">
+
+END;
+	if ($inFormName == '') {
+		$str .= sffFormDropdownHTML();
+	} else {
+		$str .= '			<input type="hidden" name="form" value="' . $inFormName . '">' . "\n";
+	}
+	// recreate the passed-in query string as a set of hidden variables
+	$query_components = explode('&', $inQueryStr);
+	foreach ($query_components as $component) {
+		list($key, $val) = explode('=', $component);
+		$str .= '			<input type="hidden" name="' . $key . '" value="' . $val . '">' . "\n";
+	}
+	$button_str = ($inButtonStr != '') ? $inButtonStr : wfMsg('addoreditdata');
+	$str .= <<<END
+			<input type="submit" value="$button_str"></p>
+			</form>
+END;
+	return array($str, noparse => true);
+}
+
+?>
