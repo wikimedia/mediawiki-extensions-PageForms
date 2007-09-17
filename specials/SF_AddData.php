17962aeaae5fef247875181558d845478cdaae50
Yaron Koren <yaron@users.mediawiki.org>
2007-09-17 20:20:49 +0000

    Improved formatting, removed unnecessary return value for formHTML()

--- specials/SF_AddData.php
+++ specials/SF_AddData.php
@@ -20,17 +20,17 @@ function doSpecialAddData($query = '') {
 	$form_name = $wgRequest->getVal('form');
 	$target_name = $wgRequest->getVal('target');
 
-        // if query string did not contain these variables, try the URL
-        if (! $form_name && ! $target_name) {
-                $queryparts = explode('/', $query, 2);
-                $form_name = isset($queryparts[0]) ? $queryparts[0] : '';
-                $target_name = isset($queryparts[1]) ? $queryparts[1] : '';
-        }
+	// if query string did not contain these variables, try the URL
+	if (! $form_name && ! $target_name) {
+		$queryparts = explode('/', $query, 2);
+		$form_name = isset($queryparts[0]) ? $queryparts[0] : '';
+		$target_name = isset($queryparts[1]) ? $queryparts[1] : '';
+	}
 
 	// get contents of template
 	$form_title = Title::newFromText($form_name, SF_NS_FORM);
         // get contents of target page
-        $target_title = Title::newFromText($target_name);
+	$target_title = Title::newFromText($target_name);
 
 	// target_title should be null - we shouldn't be adding a page that
 	// already exists
@@ -64,18 +64,17 @@ function doSpecialAddData($query = '') {
 			$page_is_source = false;
 			$page_contents = null;
 		}
-		list ($form_text, $javascript_text, $title, $data_text) =
+		list ($form_text, $javascript_text, $data_text) =
 			$sfgFormPrinter->formHTML($form_definition, $form_submitted, $page_is_source, $page_contents, $page_title);
 		if ($form_submitted) {
-			if (! $title) {$title = $target_name;}
-			$text = $sfgFormPrinter->redirectText($title, $data_text);
+			$text = $sfgFormPrinter->redirectText($target_name, $data_text);
 		} else {
 			$text =<<<END
 				<form name="createbox" onsubmit="return validate_all()" action="" method="post" class="createbox">
 
 END;
 			$text .= $form_text;
-			$target_title = Title::newFromText($title);
+			$target_title = Title::newFromText($page_title);
 			$text .= $sfgFormPrinter->formBottom($target_title, $sfgIsDisabled);
 		}
 	}
