5b56e674f40e6fc6e2f43fe195ab685fd0071866
Yaron Koren <yaron@users.mediawiki.org>
2007-10-26 19:32:39 +0000

    Much functionality has been moved to SF_GlobalFunctions.php

--- includes/SF_FormEditTab.php
+++ includes/SF_FormEditTab.php
@@ -3,28 +3,6 @@ global $wgHooks;
 $wgHooks[ 'SkinTemplateTabs' ][] = 'sffFormEditTab';
 
 /**
- * Gets names of categories for this page;
- * based on Title::getParentCategories(), but simpler
- */
-function getCategoriesForArticle($article) {
-  $fname = 'SFFormEditTab::getCategoriesForArticle()';
-  $categories = array();
-  $titlekey = $article->mTitle->getArticleId();
-  $db = wfGetDB( DB_SLAVE );
-  $conditions = "cl_from='$titlekey'";
-  $res = $db->select( $db->tableName('categorylinks'),
-                      'cl_to',
-                      $conditions, $fname);
-  if ($db->numRows( $res ) > 0) {
-    while ($row = $db->fetchRow($res)) {
-      $categories[] = $row[0];
-    }
-  }
-  $db->freeResult($res);
-  return $categories;
-}
-
-/**
  * Adds an action that uses the default form for either a category that this
  * article belongs to (if there is one), or the article's namespace, to
  * edit the article
@@ -34,35 +12,20 @@ function sffFormEditTab($obj, $content_actions) {
   // make sure that this is not itself a category page, and that the user
   // is allowed to edit it
   if (($obj->mTitle != null) && ($obj->mTitle->getNamespace() != NS_CATEGORY)) {
-    $form_name = null;
-    $categories = getCategoriesForArticle($obj);
-    $default_form_relation = str_replace(' ', '_', wfMsgForContent('sf_form_relation'));
-    foreach ($categories as $category) {
-      if ($form_name = sffGetDefaultForm($category, NS_CATEGORY)) {
-        break;
-      }
-    }
-    if (! $form_name) {
-      $form_name = sffGetDefaultForm($obj->mTitle->getNsText(), NS_PROJECT);
-    }
+    $form_name = sffGetFormForArticle($obj);
     if ($form_name) {  
-      // if this tab is appearing here due to the namespace, there's a
-      // chance that the article in question doesn't exist yet
-      if ($obj->mTitle->exists()) {
-        $page = SpecialPage::getPage('EditData');
-      } else {
-        $page = SpecialPage::getPage('AddData');
-      }
-      $target_name = sffTitleURLString($obj->mTitle);
+     global $wgRequest;
+     $class_name = ($wgRequest->getVal('action') == 'formedit') ? 'selected' : '';
 
       $content_actions['form_edit'] = array(
-        'class' => false,
+        'class' => $class_name,
         'text' => wfMsg('form_edit'),
-        'href' => $page->getTitle()->getFullURL() . "/$form_name/$target_name"
+        'href' => $obj->mTitle->getLocalURL('action=formedit')
       );
       return true;
     }
   }
   return true; // always return true, in order not to stop MW's hook processing!
 }
+
 ?>
