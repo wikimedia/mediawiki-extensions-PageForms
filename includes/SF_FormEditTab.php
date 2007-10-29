1eadc5438f76ee4e529d53ab50462c8f838b95b8
Yaron Koren <yaron@users.mediawiki.org>
2007-10-29 16:10:41 +0000

    Added handling for the new edit-tab-handling variables

--- includes/SF_FormEditTab.php
+++ includes/SF_FormEditTab.php
@@ -3,9 +3,7 @@ global $wgHooks;
 $wgHooks[ 'SkinTemplateTabs' ][] = 'sffFormEditTab';
 
 /**
- * Adds an action that uses the default form for either a category that this
- * article belongs to (if there is one), or the article's namespace, to
- * edit the article
+ * Adds an "action" (i.e., a tab) to edit the current article with a form
  */
 function sffFormEditTab($obj, $content_actions) {
   $fname = 'SFFormEditTab';
@@ -14,14 +12,46 @@ function sffFormEditTab($obj, $content_actions) {
   if (($obj->mTitle != null) && ($obj->mTitle->getNamespace() != NS_CATEGORY)) {
     $form_name = sffGetFormForArticle($obj);
     if ($form_name) {  
-     global $wgRequest;
-     $class_name = ($wgRequest->getVal('action') == 'formedit') ? 'selected' : '';
+      global $wgRequest;
+      global $sfgHideMainEditTab, $sfgSwitchEditTabLocations, $sfgRenameEditTabs;
 
-      $content_actions['form_edit'] = array(
+      // create the form edit tab, and apply whatever changes are specified
+      // by the three edit-tab global variables
+      $edit_tab_hidden = false;
+      if ($sfgHideMainEditTab) {
+        global $wgUser;
+        // TODO - there should be a better way to determine if the user is a sysop
+        if (! $wgUser->isAllowed('delete')) {
+          $edit_tab_hidden = true;
+        }
+      }
+
+      if ($sfgRenameEditTabs) {
+        $form_edit_tab_text = wfMsg('edit');
+        $content_actions['edit']['text'] = wfMsg('edit_source');
+      } else {
+        $form_edit_tab_text = wfMsg('form_edit');
+      }
+
+      $class_name = ($wgRequest->getVal('action') == 'formedit') ? 'selected' : '';
+      $form_edit_tab = array(
         'class' => $class_name,
-        'text' => wfMsg('form_edit'),
+        'text' => $form_edit_tab_text,
         'href' => $obj->mTitle->getLocalURL('action=formedit')
       );
+
+      if ($sfgSwitchEditTabLocations) {
+        if (! $edit_tab_hidden) {
+          $content_actions['form_edit'] = $content_actions['edit'];
+        }
+        $content_actions['edit'] = $form_edit_tab;
+      } else {
+        if ($edit_tab_hidden) {
+          unset($content_actions['edit']);
+        }
+        $content_actions['form_edit'] = $form_edit_tab;
+      }
+
       return true;
     }
   }
