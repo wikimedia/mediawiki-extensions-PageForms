b029eef8350fc27c818c95b2e27ac2732c2af8a2
Yaron Koren <yaron@users.mediawiki.org>
2007-10-29 16:11:21 +0000

    New version - 0.7.1

--- includes/SF_GlobalFunctions.php
+++ includes/SF_GlobalFunctions.php
@@ -7,7 +7,7 @@
  * @author Louis Gerbarg
  */
 
-define('SF_VERSION','0.7');
+define('SF_VERSION','0.7.1');
 
 $wgExtensionFunctions[] = 'sfgSetupExtension';
 $wgExtensionFunctions[] = 'sfgParserFunctions';
