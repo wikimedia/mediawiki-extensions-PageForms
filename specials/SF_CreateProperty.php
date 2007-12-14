d639c497e2e4e23065cbf74b5d1f5ac7849bcd43
Yaron Koren <yaron@users.mediawiki.org>
2007-12-14 16:14:14 +0000

    Fixed PHP "notice" error

--- specials/SF_CreateProperty.php
+++ specials/SF_CreateProperty.php
@@ -108,7 +108,7 @@ function doSpecialCreateProperty() {
       $full_text = createPropertyText($property_type, $allowed_values);
       // HTML-encode
       $full_text = str_replace('"', '&quot;', $full_text);
-      $text .= sffPrintRedirectForm($title, $full_text, "", $save_page, $preview_page, false, false, false);
+      $text = sffPrintRedirectForm($title, $full_text, "", $save_page, $preview_page, false, false, false);
       $wgOut->addHTML($text);
       return;
     }
