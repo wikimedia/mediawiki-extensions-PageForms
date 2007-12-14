39dcf9107e94b9821c61e1b8359d20de5fbd2cd3
Yaron Koren <yaron@users.mediawiki.org>
2007-12-14 17:18:45 +0000

    Fixed PHP "notice" error

--- specials/SF_CreateCategory.php
+++ specials/SF_CreateCategory.php
@@ -80,7 +80,7 @@ function doSpecialCreateCategory() {
       $full_text = createCategoryText($default_form, $category_name, $parent_category);
       // HTML-encode
       $full_text = str_replace('"', '&quot;', $full_text);
-      $text .= sffPrintRedirectForm($title, $full_text, "", $save_page, $preview_page, false, false, false);
+      $text = sffPrintRedirectForm($title, $full_text, "", $save_page, $preview_page, false, false, false);
       $wgOut->addHTML($text);
       return;
     }
