27f3ef5b34b0298a13dc49e9e73ad7ae3c30c217
Yaron Koren <yaron@users.mediawiki.org>
2008-01-03 21:44:48 +0000

    Improved formatting, fixed bug that made these languages inoperable in SMW 1.0

--- languages/SF_LanguageZh_cn.php
+++ languages/SF_LanguageZh_cn.php
@@ -5,18 +5,18 @@
  
 class SF_LanguageZh_cn extends SF_Language {
 
-/* private */ var $m_SpecialProperties = array(
-	//always start upper-case
-  SF_SP_HAS_DEFAULT_FORM  => '预设表单',	//(Has default form) 
-	SF_SP_HAS_ALTERNATE_FORM  => '代用表单'  //(Has alternate form)
-);
+	/* private */ var $m_SpecialProperties = array(
+		//always start upper-case
+		SF_SP_HAS_DEFAULT_FORM  => '预设表单',	//(Has default form) 
+		SF_SP_HAS_ALTERNATE_FORM  => '代用表单'  //(Has alternate form)
+	);
+
+	var $m_Namespaces = array(
+		SF_NS_FORM           => '表单',		//	(Form)
+		SF_NS_FORM_TALK      => '表单讨论'	//	(Form_talk)  SF 0.7.6前原译为'表单_talk'
+	);
 
 $m_SpecialPropertyAliases['设有表单'] = SF_SP_HAS_DEFAULT_FORM;	//(Has default form) //Adding the item "Has alternate form", this item will not be suitable for translating into “设有表单＂. It has changed to use “预设表单＂. 
-
-var $m_Namespaces = array(
-	SF_NS_FORM           => '表单',		//	(Form)
-	SF_NS_FORM_TALK      => '表单讨论'	//	(Form_talk)  SF 0.7.6前原译为'表单_talk'
-);
 
 }
 
