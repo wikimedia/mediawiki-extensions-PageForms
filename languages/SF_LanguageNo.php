0363e4ef52495338dfcb473b84fd5b99610e6071
Jon Harald Søby <jhsoby@users.mediawiki.org>
2008-08-01 17:24:08 +0000

    Adding a file for Norwegian as well (SemanticForms)

---
+++ languages/SF_LanguageNo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Jon Harald Søby
+ */
+
+class SF_LanguageEn extends SF_Language {
+
+	/* private */ var $m_SpecialProperties = array(
+		// always start upper-case
+		SF_SP_HAS_DEFAULT_FORM  => 'Har standardskjema',
+		SF_SP_HAS_ALTERNATE_FORM  => 'Har alternativt skjema'
+	);
+
+	var $m_Namespaces = array(
+		SF_NS_FORM           => 'Skjema',
+		SF_NS_FORM_TALK      => 'Skjemadiskusjon'
+	);
+
+}
+
+?>
