b5e13dc4cb8a81e53bd38dfe20612dd59f61896f
Yaron Koren <yaron@users.mediawiki.org>
2008-08-14 16:48:00 +0000

    Improved formatting of comments

--- includes/SF_FormEditTab.php
+++ includes/SF_FormEditTab.php
@@ -16,8 +16,8 @@ function sffFormEditTab($obj, $content_actions) {
 			global $sfgRenameEditTabs;
 
 			$user_can_edit = $wgUser->isAllowed('edit') && $obj->mTitle->userCanEdit();
-			// create the form edit tab, and apply whatever changes are specified
-			// by the edit-tab global variables
+			// create the form edit tab, and apply whatever changes
+			// are specified by the edit-tab global variables
 			if ($sfgRenameEditTabs) {
 				$form_edit_tab_text = $user_can_edit ? wfMsg('edit') : wfMsg('sf_viewform');
 				if (array_key_exists('edit', $content_actions)) {
@@ -34,11 +34,11 @@ function sffFormEditTab($obj, $content_actions) {
 				'href' => $obj->mTitle->getLocalURL('action=formedit')
 			);
 
-			// find the location of the 'edit' tab, and add 'edit with form'
-			// right before it.
-			// this is a "key-safe" splice - it preserves both the keys and
-			// the values of the array, by editing them separately and then
-			// rebuilding the array.
+			// find the location of the 'edit' tab, and add 'edit
+			// with form' right before it.
+			// this is a "key-safe" splice - it preserves both the
+			// keys and the values of the array, by editing them
+			// separately and then rebuilding the array.
 			// based on the example at http://us2.php.net/manual/en/function.array-splice.php#31234
 			$tab_keys = array_keys($content_actions);
 			$tab_values = array_values($content_actions);
@@ -46,8 +46,9 @@ function sffFormEditTab($obj, $content_actions) {
 			// if there's no 'edit' tab, look for the 'view source' tab instead
 			if ($edit_tab_location == NULL)
 				$edit_tab_location = array_search('viewsource', $tab_keys);
-			// this should rarely happen, but if there was no edit *or* view source
-			// tab, set the location index to -1, so the tab shows up near the end
+			// this should rarely happen, but if there was no edit
+			// *or* view source tab, set the location index to -1,
+			// so the tab shows up near the end
 			if ($edit_tab_location == NULL)
 				$edit_tab_location = -1;
 			array_splice($tab_keys, $edit_tab_location, 0, 'form_edit');
