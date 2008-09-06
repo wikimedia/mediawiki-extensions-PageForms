42a03a77131f745ca9efae75552ade888ac09f58
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-09-06 23:14:53 +0000

    Localisation updates for extension messages from Betawiki (2008-09-07 00:39 CEST)

--- languages/SF_Aliases.php
+++ languages/SF_Aliases.php
@@ -21,3 +21,46 @@ $aliases['en'] = array(
 	'Templates' => array( 'Templates' ),
 	'UploadWindow' => array( 'UploadWindow' ),
 );
+
+/** Arabic (العربية) */
+$aliases['ar'] = array(
+	'AddData' => array( 'إضافة_بيانات' ),
+	'AddPage' => array( 'إضافة_صفحة' ),
+	'CreateCategory' => array( 'إنشاء_تصنيف' ),
+	'CreateForm' => array( 'إنشاء_استمارة' ),
+	'CreateProperty' => array( 'إنشاء_خاصية' ),
+	'CreateTemplate' => array( 'إنشاء_قالب' ),
+	'EditData' => array( 'تعديل_البيانات' ),
+	'Forms' => array( 'استمارات' ),
+	'Templates' => array( 'قوالب' ),
+	'UploadWindow' => array( 'نافذة_الرفع' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$aliases['arz'] = array(
+	'AddData' => array( 'إضافة_بيانات' ),
+	'AddPage' => array( 'إضافة_صفحة' ),
+	'CreateCategory' => array( 'إنشاء_تصنيف' ),
+	'CreateForm' => array( 'إنشاء_استمارة' ),
+	'CreateProperty' => array( 'إنشاء_خاصية' ),
+	'CreateTemplate' => array( 'إنشاء_قالب' ),
+	'EditData' => array( 'تعديل_البيانات' ),
+	'Forms' => array( 'استمارات' ),
+	'Templates' => array( 'قوالب' ),
+	'UploadWindow' => array( 'نافذة_الرفع' ),
+);
+
+/** Dutch (Nederlands) */
+$aliases['nl'] = array(
+	'AddData' => array( 'GegevensToevoegen' ),
+	'AddPage' => array( 'PaginaToevoegen' ),
+	'CreateCategory' => array( 'CategorieAanmaken' ),
+	'CreateForm' => array( 'FormulierAanmaken' ),
+	'CreateProperty' => array( 'EigenschapAanmaken' ),
+	'CreateTemplate' => array( 'SjabloonAanmaken' ),
+	'EditData' => array( 'GegevensBewerken' ),
+	'Forms' => array( 'Formulieren' ),
+	'Templates' => array( 'Sjablonen' ),
+	'UploadWindow' => array( 'Uploadscherm' ),
+);
+
