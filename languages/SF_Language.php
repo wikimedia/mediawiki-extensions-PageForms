672886d6b527b51a17a37ed213c0f548253c85f1
Yaron Koren <yaron@users.mediawiki.org>
2008-12-10 15:22:02 +0000

    Changed function names to be in line with SMW's

--- languages/SF_Language.php
+++ languages/SF_Language.php
@@ -43,14 +43,14 @@ abstract class SF_Language {
 	/**
 	 * Function that returns the labels for the special properties.
 	 */
-	function getSpecialPropertiesArray() {
+	function getPropertyLabels() {
 		return $this->m_SpecialProperties;
 	}
 
 	/**
 	 * Aliases for special properties, if any.
 	 */
-	function getSpecialPropertyAliases() {
+	function getPropertyAliases() {
 		return $this->m_SpecialPropertyAliases;
 	}
 
