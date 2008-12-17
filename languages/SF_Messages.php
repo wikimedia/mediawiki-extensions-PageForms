934d15d04d7e8c8e2ce94080cdc24616af46afdf
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-12-17 23:51:14 +0000

    Localisation updates for extension messages from Betawiki (2008-12-18 00:40 CET)

--- languages/SF_Messages.php
+++ languages/SF_Messages.php
@@ -3422,6 +3422,7 @@ $messages['stq'] = array(
  * @author Najami
  */
 $messages['sv'] = array(
+	'semanticforms-desc' => 'Formulär för att lägga till och redigera semantisk data',
 	'specialpages-group-sf_group' => 'Semantiska formulär',
 	'createproperty' => 'Skapa en egenskap',
 	'sf_createproperty_allowedvalsinput' => 'Om du önskar att detta fält endast ska kunna ha vissa värden, skriv in listan över tillåtna värden, separerade med komma (om ett värde innehåller komma, ersätt det med "\\,"):',
