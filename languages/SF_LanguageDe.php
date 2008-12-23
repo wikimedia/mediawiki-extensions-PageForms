eabc0267ea59b989d7a953605f5b0c73c061f944
Yaron Koren <yaron@users.mediawiki.org>
2008-12-23 16:57:17 +0000

    Typo fix for 'alternate form' property

--- languages/SF_LanguageDe.php
+++ languages/SF_LanguageDe.php
@@ -8,7 +8,7 @@ class SF_LanguageDe extends SF_Language {
 	/* private */ var $m_SpecialProperties = array(
 		//always start upper-case
 		SF_SP_HAS_DEFAULT_FORM    => 'Hat Standardformular',
-		SF_SP_HAS_ALTERNATE_FORM  => 'Hat Alternativeformular'
+		SF_SP_HAS_ALTERNATE_FORM  => 'Hat Alternativformular'
 	);
 	
 	var $m_Namespaces = array(
