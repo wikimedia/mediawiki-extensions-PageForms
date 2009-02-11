1c6cba80f53e6ae35560eb7e85219ba5a4b92f9e
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-02-11 18:38:18 +0000

    Localisation updates for extension messages from translatewiki.net (2009-02-11 18:17 UTC)

--- languages/SF_Messages.php
+++ languages/SF_Messages.php
@@ -488,6 +488,7 @@ $messages['be-tarask'] = array(
 	'sf_createproperty_propname' => 'Назва:',
 	'sf_createproperty_proptype' => 'Тып:',
 	'sf_createtemplate_deletefield' => 'Выдаліць',
+	'sf_createform_inputtypedefault' => '(па змоўчваньні)',
 	'sf_createform_add' => 'Дадаць',
 	'sf_createcategory_name' => 'Назва:',
 	'sf_editdata_remove' => 'Выдаліць',
