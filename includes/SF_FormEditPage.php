d0c5ffe18d55083354050b97640499bde32b579b
Yaron Koren <yaron@users.mediawiki.org>
2009-04-21 16:47:13 +0000

    Various improvements for eventual usage

--- includes/SF_FormEditPage.php
+++ includes/SF_FormEditPage.php
@@ -3,12 +3,13 @@
  * Form Edit Page inheriting from EditPage
  * 
  * @author Daniel Friesen
+ * @author Yaron Koren
  */
 
 class FormEditPage extends EditPage {
-	
+
 	protected $form, $form_name;
-	
+
 	function __construct( $article, $form_name = '' ) {
 		global $wgRequest;
 		parent::__construct( $article );
@@ -18,7 +19,7 @@ class FormEditPage extends EditPage {
 		$this->form = Title::makeTitleSafe(SF_NS_FORM, $form_name);
 		$this->form_name = $form_name;
 	}
-	
+
 	function setHeaders() {
 		parent::setHeaders();
 		global $wgOut, $wgTitle;
@@ -27,7 +28,12 @@ class FormEditPage extends EditPage {
 				$this->form->getText(), $wgTitle->getPrefixedText() ) );
 		}
 	}
-	
+
+	protected function displayPreviewArea( $previewOutput, $isOnTop = false ) {
+		if ($this->textbox1 != null)
+			parent::displayPreviewArea($previewOutput);
+	}
+
 	protected function showTextbox1( $classes ) {
 		if( $this->isConflict ) {
 			// Fallback to normal mode when showing an editconflict
@@ -38,10 +44,13 @@ class FormEditPage extends EditPage {
 		$target_title = $this->mArticle->getTitle();
 		$target_name = SFLinkUtils::titleString($target_title);
 		if ($target_title->exists()) {
-			SFEditData::printEditForm($this->form_name, $target_name);
+			SFEditData::printEditForm($this->form_name, $target_name, $this->textbox1);
 		} else {
-			SFAddData::printAddForm($this->form_name, $target_name, array());
+			SFAddData::printAddForm($this->form_name, $target_name, array(), $this->textbox1);
 		}
 
+	}
+
+	function showFooter() {
 	}
 }
