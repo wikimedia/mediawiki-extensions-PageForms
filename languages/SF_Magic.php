78e7b777c7d0d2b79150d7fd99dd754aa5b8ab45
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-08-12 15:28:41 +0000

    Really fix syntax error from r54866

--- languages/SF_Magic.php
+++ languages/SF_Magic.php
@@ -2,7 +2,7 @@
 
 $magicWords = array();
 
-$magicWords['en'] => array(
+$magicWords['en'] = array(
 	'forminput'        => array ( 0, 'forminput' ),
 	'formlink'         => array ( 0, 'formlink' ),
 	'arraymap'         => array ( 0, 'arraymap' ),
