97d177fadf1315325d03bcc42504dceaed191906
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-08-14 14:19:47 +0000

    Localisation updates for extension magic words from translatewiki.net

--- languages/SF_Magic.php
+++ languages/SF_Magic.php
@@ -8,3 +8,14 @@ $magicWords['en'] = array(
 	'arraymap'         => array ( 0, 'arraymap' ),
 	'arraymaptemplate' => array ( 0, 'arraymaptemplate' )
 );
+
+$magicWords['ar'] = array(
+	'arraymap'           => array( '0', 'خريطة_المصفوفة', 'arraymap' ),
+	'arraymaptemplate'   => array( '0', 'قالب_خريطة_المصفوفة', 'arraymaptemplate' ),
+);
+
+$magicWords['nl'] = array(
+	'forminput'          => array( '0', 'formulierinvoer', 'forminput' ),
+	'formlink'           => array( '0', 'formulierverwijzing', 'formlink' ),
+	'arraymaptemplate'   => array( '0', 'arraymapsjabloon', 'arraymaptemplate' ),
+);
