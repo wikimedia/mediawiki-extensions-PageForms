53fd20e0bcef535b50ad94ae8f2c005047e32d77
Yaron Koren <yaron@users.mediawiki.org>
2010-03-09 16:10:21 +0000

    Changed again, to match Uwe Baumbach's actual fix

--- specials/SF_Templates.php
+++ specials/SF_Templates.php
@@ -72,7 +72,7 @@ class TemplatesPage extends QueryPage {
 
 		$template_text = $template_article->getContent();
 		$cat_ns_name = $wgContLang->getNsText(14);
-		if (preg_match_all("/\[\[$cat_ns_name:(.*)\]\]/", $template_text, $matches)) {
+		if (preg_match_all("/\[\[(Category|$cat_ns_name):(.*)\]\]/", $template_text, $matches)) {
 			// get the last match - if there's more than one
 			// category tag, there's a good chance that the last
 			// one will be the relevant one - the others are
