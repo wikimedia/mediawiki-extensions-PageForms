56aeee4abf90b8be48a3f675e5af99f0989f885e
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2010-03-14 18:01:44 +0000

    Stylized with Stylize.php to conform with MW's spacing conventions

--- includes/SF_GlobalFunctions.php
+++ includes/SF_GlobalFunctions.php
@@ -7,9 +7,9 @@
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define('SF_VERSION','1.9');
+define( 'SF_VERSION', '1.9' );
 
-$wgExtensionCredits['specialpage'][]= array(
+$wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'Semantic Forms',
 	'version' => SF_VERSION,
@@ -19,11 +19,11 @@ $wgExtensionCredits['specialpage'][]= array(
 );
 
 // constants for special properties
-define('SF_SP_HAS_DEFAULT_FORM', 1);
-define('SF_SP_HAS_ALTERNATE_FORM', 2);
-define('SF_SP_CREATES_PAGES_WITH_FORM', 3);
-define('SF_SP_PAGE_HAS_DEFAULT_FORM', 4);
-define('SF_SP_HAS_FIELD_LABEL_FORMAT', 5);
+define( 'SF_SP_HAS_DEFAULT_FORM', 1 );
+define( 'SF_SP_HAS_ALTERNATE_FORM', 2 );
+define( 'SF_SP_CREATES_PAGES_WITH_FORM', 3 );
+define( 'SF_SP_PAGE_HAS_DEFAULT_FORM', 4 );
+define( 'SF_SP_HAS_FIELD_LABEL_FORMAT', 5 );
 
 $wgExtensionFunctions[] = 'sfgSetupExtension';
 $wgExtensionFunctions[] = 'sfgParserFunctions';
@@ -77,7 +77,7 @@ $wgSpecialPages['RunQuery'] = 'SFRunQuery';
 $wgAutoloadClasses['SFRunQuery'] = $sfgIP . '/specials/SF_RunQuery.php';
 $wgSpecialPageGroups['RunQuery'] = 'sf_group';
 // different upload-window class for MW 1.16+
-if (class_exists('HTMLTextField')) { // added in MW 1.16
+if ( class_exists( 'HTMLTextField' ) ) { // added in MW 1.16
 	$wgSpecialPages['UploadWindow'] = 'SFUploadWindow2';
 	$wgAutoloadClasses['SFUploadWindow2'] = $sfgIP . '/specials/SF_UploadWindow2.php';
 } else {
@@ -99,7 +99,7 @@ $wgAutoloadClasses['SFParserFunctions'] = $sfgIP . '/includes/SF_ParserFunctions
 $wgAutoloadClasses['SFAutocompleteAPI'] = $sfgIP . '/includes/SF_AutocompleteAPI.php';
 $wgJobClasses['createPage'] = 'SFCreatePageJob';
 $wgAutoloadClasses['SFCreatePageJob'] = $sfgIP . '/includes/SF_CreatePageJob.php';
-require_once($sfgIP . '/languages/SF_Language.php');
+require_once( $sfgIP . '/languages/SF_Language.php' );
 
 $wgExtensionMessagesFiles['SemanticForms'] = $sfgIP . '/languages/SF_Messages.php';
 $wgExtensionAliasesFiles['SemanticForms'] = $sfgIP . '/languages/SF_Aliases.php';
@@ -118,7 +118,7 @@ function sfgSetupExtension() {
 
 function sfgParserFunctions() {
 	global $wgHooks, $wgParser;
-	if( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
+	if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
 		$wgHooks['ParserFirstCallInit'][] = 'SFParserFunctions::registerFunctions';
 	} else {
 		if ( class_exists( 'StubObject' ) && !StubObject::isRealObject( $wgParser ) ) {
@@ -140,10 +140,10 @@ function sfgParserFunctions() {
 function sffInitNamespaces() {
 	global $wgExtraNamespaces, $wgNamespaceAliases, $wgNamespacesWithSubpages, $wgLanguageCode, $sfgContLang;
 
-	sffInitContentLanguage($wgLanguageCode);
+	sffInitContentLanguage( $wgLanguageCode );
 
 	// Register namespace identifiers
-	if (!is_array($wgExtraNamespaces)) { $wgExtraNamespaces=array(); }
+	if ( !is_array( $wgExtraNamespaces ) ) { $wgExtraNamespaces = array(); }
 	$wgExtraNamespaces = $wgExtraNamespaces + $sfgContLang->getNamespaces();
 	$wgNamespaceAliases = $wgNamespaceAliases + $sfgContLang->getNamespaceAliases();
 
@@ -163,19 +163,19 @@ function sffInitNamespaces() {
  * determine labels for additional namespaces. In contrast, messages
  * can be initialised much later, when they are actually needed.
  */
-function sffInitContentLanguage($langcode) {
+function sffInitContentLanguage( $langcode ) {
 	global $sfgIP, $sfgContLang;
 
-	if (!empty($sfgContLang)) { return; }
+	if ( !empty( $sfgContLang ) ) { return; }
 
 	$cont_lang_class = 'SF_Language' . str_replace( '-', '_', ucfirst( $langcode ) );
-	if (file_exists($sfgIP . '/languages/'. $cont_lang_class . '.php')) {
-		include_once( $sfgIP . '/languages/'. $cont_lang_class . '.php' );
+	if ( file_exists( $sfgIP . '/languages/' . $cont_lang_class . '.php' ) ) {
+		include_once( $sfgIP . '/languages/' . $cont_lang_class . '.php' );
 	}
 
 	// fallback if language not supported
-	if ( !class_exists($cont_lang_class)) {
-		include_once($sfgIP . '/languages/SF_LanguageEn.php');
+	if ( !class_exists( $cont_lang_class ) ) {
+		include_once( $sfgIP . '/languages/SF_LanguageEn.php' );
 		$cont_lang_class = 'SF_LanguageEn';
 	}
 
@@ -187,19 +187,19 @@ function sffInitContentLanguage($langcode) {
  * must happen after the content language was initialised, since
  * this language is used as a fallback.
  */
-function sffInitUserLanguage($langcode) {
+function sffInitUserLanguage( $langcode ) {
 	global $sfgIP, $sfgLang;
 
-	if (!empty($sfgLang)) { return; }
+	if ( !empty( $sfgLang ) ) { return; }
 
 	$sfLangClass = 'SF_Language' . str_replace( '-', '_', ucfirst( $langcode ) );
 
-	if (file_exists($sfgIP . '/languages/'. $sfLangClass . '.php')) {
-		include_once( $sfgIP . '/languages/'. $sfLangClass . '.php' );
+	if ( file_exists( $sfgIP . '/languages/' . $sfLangClass . '.php' ) ) {
+		include_once( $sfgIP . '/languages/' . $sfLangClass . '.php' );
 	}
 
 	// fallback if language not supported
-	if ( !class_exists($sfLangClass)) {
+	if ( !class_exists( $sfLangClass ) ) {
 		global $sfgContLang;
 		$sfgLang = $sfgContLang;
 	} else {
@@ -207,24 +207,24 @@ function sffInitUserLanguage($langcode) {
 	}
 }
 
-function sffAddToAdminLinks(&$admin_links_tree) {
-	$data_structure_label = wfMsg('smw_adminlinks_datastructure');
-	$data_structure_section = $admin_links_tree->getSection($data_structure_label);
-	if (is_null($data_structure_section))
+function sffAddToAdminLinks( &$admin_links_tree ) {
+	$data_structure_label = wfMsg( 'smw_adminlinks_datastructure' );
+	$data_structure_section = $admin_links_tree->getSection( $data_structure_label );
+	if ( is_null( $data_structure_section ) )
 		return true;
-	$smw_row = $data_structure_section->getRow('smw');
-	$smw_row->addItem(ALItem::newFromSpecialPage('Templates'), 'Properties');
-	$smw_row->addItem(ALItem::newFromSpecialPage('Forms'), 'SemanticStatistics');
-	$smw_admin_row = $data_structure_section->getRow('smw_admin');
-	$smw_admin_row->addItem(ALItem::newFromSpecialPage('CreateClass'), 'SMWAdmin');
-	$smw_admin_row->addItem(ALItem::newFromSpecialPage('CreateProperty'), 'SMWAdmin');
-	$smw_admin_row->addItem(ALItem::newFromSpecialPage('CreateTemplate'), 'SMWAdmin');
-	$smw_admin_row->addItem(ALItem::newFromSpecialPage('CreateForm'), 'SMWAdmin');
-	$smw_admin_row->addItem(ALItem::newFromSpecialPage('CreateCategory'), 'SMWAdmin');
-	$smw_docu_row = $data_structure_section->getRow('smw_docu');
-	$sf_name = wfMsg('specialpages-group-sf_group');
-	$sf_docu_label = wfMsg('adminlinks_documentation', $sf_name);
-	$smw_docu_row->addItem(ALItem::newFromExternalLink("http://www.mediawiki.org/wiki/Extension:Semantic_Forms", $sf_docu_label));
+	$smw_row = $data_structure_section->getRow( 'smw' );
+	$smw_row->addItem( ALItem::newFromSpecialPage( 'Templates' ), 'Properties' );
+	$smw_row->addItem( ALItem::newFromSpecialPage( 'Forms' ), 'SemanticStatistics' );
+	$smw_admin_row = $data_structure_section->getRow( 'smw_admin' );
+	$smw_admin_row->addItem( ALItem::newFromSpecialPage( 'CreateClass' ), 'SMWAdmin' );
+	$smw_admin_row->addItem( ALItem::newFromSpecialPage( 'CreateProperty' ), 'SMWAdmin' );
+	$smw_admin_row->addItem( ALItem::newFromSpecialPage( 'CreateTemplate' ), 'SMWAdmin' );
+	$smw_admin_row->addItem( ALItem::newFromSpecialPage( 'CreateForm' ), 'SMWAdmin' );
+	$smw_admin_row->addItem( ALItem::newFromSpecialPage( 'CreateCategory' ), 'SMWAdmin' );
+	$smw_docu_row = $data_structure_section->getRow( 'smw_docu' );
+	$sf_name = wfMsg( 'specialpages-group-sf_group' );
+	$sf_docu_label = wfMsg( 'adminlinks_documentation', $sf_name );
+	$smw_docu_row->addItem( ALItem::newFromExternalLink( "http://www.mediawiki.org/wiki/Extension:Semantic_Forms", $sf_docu_label ) );
 
 	return true;
 }
--- specials/SF_CreateClass.php
+++ specials/SF_CreateClass.php
@@ -6,7 +6,7 @@
  * @author Yaron Koren
  */
 
-if (!defined('MEDIAWIKI')) die();
+if ( !defined( 'MEDIAWIKI' ) ) die();
 
 class SFCreateClass extends SpecialPage {
 
@@ -17,7 +17,7 @@ class SFCreateClass extends SpecialPage {
 		parent::__construct( 'CreateClass', 'createclass' );
 	}
 
-	function execute($query) {
+	function execute( $query ) {
 		global $wgOut, $wgRequest, $wgUser, $sfgScriptPath;
 		global $wgLang, $smwgContLang;
 
@@ -28,44 +28,44 @@ class SFCreateClass extends SpecialPage {
                 }
 
 		$this->setHeaders();
-		wfLoadExtensionMessages('SemanticForms');
+		wfLoadExtensionMessages( 'SemanticForms' );
 
-		$create_button_text = wfMsg('create');
+		$create_button_text = wfMsg( 'create' );
 
 		$property_name_error_str = '';
-		$save_page = $wgRequest->getCheck('wpSave');
-		if ($save_page) {
-			$template_name = trim($wgRequest->getVal("template_name"));
-			$form_name = trim($wgRequest->getVal("form_name"));
-			$category_name = trim($wgRequest->getVal("category_name"));
-			if ($template_name == '' | $form_name == '' || $category_name == '') {
-				$text = Xml::element('p', null, wfMsg('sf_createclass_missingvalues'));
-				$wgOut->addHTML($text);
+		$save_page = $wgRequest->getCheck( 'wpSave' );
+		if ( $save_page ) {
+			$template_name = trim( $wgRequest->getVal( "template_name" ) );
+			$form_name = trim( $wgRequest->getVal( "form_name" ) );
+			$category_name = trim( $wgRequest->getVal( "category_name" ) );
+			if ( $template_name == '' | $form_name == '' || $category_name == '' ) {
+				$text = Xml::element( 'p', null, wfMsg( 'sf_createclass_missingvalues' ) );
+				$wgOut->addHTML( $text );
 				return;
 			}
 			$fields = array();
 			$jobs = array();
 			// cycle through all the rows passed in
-			for ($i = 1; $wgRequest->getCheck("property_name_$i"); $i++) {
+			for ( $i = 1; $wgRequest->getCheck( "property_name_$i" ); $i++ ) {
 				// go through the query values, setting the appropriate local variables
-				$property_name = trim($wgRequest->getVal("property_name_$i"));
-				if (empty($property_name)) continue;
-				$field_name = trim($wgRequest->getVal("field_name_$i"));
-				if ($field_name === '')
+				$property_name = trim( $wgRequest->getVal( "property_name_$i" ) );
+				if ( empty( $property_name ) ) continue;
+				$field_name = trim( $wgRequest->getVal( "field_name_$i" ) );
+				if ( $field_name === '' )
 					$field_name = $property_name;
-				$property_type = $wgRequest->getVal("property_type_$i");
-				$allowed_values = $wgRequest->getVal("allowed_values_$i");
-				$is_list = $wgRequest->getCheck("is_list_$i");
+				$property_type = $wgRequest->getVal( "property_type_$i" );
+				$allowed_values = $wgRequest->getVal( "allowed_values_$i" );
+				$is_list = $wgRequest->getCheck( "is_list_$i" );
 				// create an SFTemplateField based on these
 				// values, and add it to the $fields array
-				$field = SFTemplateField::create($field_name, $field_name);
+				$field = SFTemplateField::create( $field_name, $field_name );
 				$field->semantic_property = $property_name;
 				$field->is_list = $is_list;
 				$fields[] = $field;
 
 				// create the property, and make a job for it
-				$full_text = SFCreateProperty::createPropertyText($property_type, '', $allowed_values);
-				$property_title = Title::makeTitleSafe(SMW_NS_PROPERTY, $property_name);
+				$full_text = SFCreateProperty::createPropertyText( $property_type, '', $allowed_values );
+				$property_title = Title::makeTitleSafe( SMW_NS_PROPERTY, $property_name );
 				$params = array();
 				$params['user_id'] = $wgUser->getId();
 				$params['page_text'] = $full_text;
@@ -73,34 +73,34 @@ class SFCreateClass extends SpecialPage {
 			}
 
 			// create the template, and save it
-			$full_text = SFTemplateField::createTemplateText($template_name, $fields, $category_name, '', '', '');
-			$template_title = Title::makeTitleSafe(NS_TEMPLATE, $template_name);
-			$template_article = new Article($template_title);
+			$full_text = SFTemplateField::createTemplateText( $template_name, $fields, $category_name, '', '', '' );
+			$template_title = Title::makeTitleSafe( NS_TEMPLATE, $template_name );
+			$template_article = new Article( $template_title );
 			$edit_summary = '';
-			$template_article->doEdit($full_text, $edit_summary);
+			$template_article->doEdit( $full_text, $edit_summary );
 
 			// create the form, and make a job for it
-			$form_template = SFTemplateInForm::create($template_name, '', false);
-			$form_templates = array($form_template);
-			$form = SFForm::create($form_name, $form_templates);
+			$form_template = SFTemplateInForm::create( $template_name, '', false );
+			$form_templates = array( $form_template );
+			$form = SFForm::create( $form_name, $form_templates );
 			$full_text = $form->createMarkup();
-			$form_title = Title::makeTitleSafe(SF_NS_FORM, $form_name);
+			$form_title = Title::makeTitleSafe( SF_NS_FORM, $form_name );
 			$params = array();
 			$params['user_id'] = $wgUser->getId();
 			$params['page_text'] = $full_text;
 			$jobs[] = new SFCreatePageJob( $form_title, $params );
 
 			// create the category, and make a job for it
-			$full_text = SFCreateCategory::createCategoryText($form_name, $category_name, '');
-			$category_title = Title::makeTitleSafe(NS_CATEGORY, $category_name);
+			$full_text = SFCreateCategory::createCategoryText( $form_name, $category_name, '' );
+			$category_title = Title::makeTitleSafe( NS_CATEGORY, $category_name );
 			$params = array();
 			$params['user_id'] = $wgUser->getId();
 			$params['page_text'] = $full_text;
 			$jobs[] = new SFCreatePageJob( $category_title, $params );
 			Job::batchInsert( $jobs );
 
-			$text = Xml::element('p', null, wfMsg('sf_createclass_success'));
-			$wgOut->addHTML($text);
+			$text = Xml::element( 'p', null, wfMsg( 'sf_createclass_success' ) );
+			$wgOut->addHTML( $text );
 			return;
 		}
 
@@ -115,25 +115,25 @@ class SFCreateClass extends SpecialPage {
 		// link to them at the top of the page
 		$sk = $wgUser->getSkin();
 		$creation_links = array();
-		$cp = SpecialPage::getPage('CreateProperty');
-		$creation_links[] = $sk->makeKnownLinkObj($cp->getTitle(), $cp->getDescription());
-		$ct = SpecialPage::getPage('CreateTemplate');
-		$creation_links[] = $sk->makeKnownLinkObj($ct->getTitle(), $ct->getDescription());
-		$cf = SpecialPage::getPage('CreateForm');
-		$creation_links[] = $sk->makeKnownLinkObj($cf->getTitle(), $cf->getDescription());
-		$cc = SpecialPage::getPage('CreateCategory');
-		$creation_links[] = $sk->makeKnownLinkObj($cc->getTitle(), $cc->getDescription());
-		$create_class_docu = wfMsg('sf_createclass_docu', $wgLang->listToText($creation_links));
-		$leave_field_blank = wfMsg('sf_createclass_leavefieldblank');
-		$form_name_label = wfMsg('sf_createform_nameinput');
-		$template_name_label = wfMsg('sf_createtemplate_namelabel');
-		$category_name_label = wfMsg('sf_createcategory_name');
-		$property_name_label = wfMsg('sf_createproperty_propname');
-		$field_name_label = wfMsg('sf_createtemplate_fieldname');
-		$type_label = wfMsg('sf_createproperty_proptype');
-		$allowed_values_label = wfMsg('sf_createclass_allowedvalues') . wfMsg('colon-separator');
-		$list_of_values_label = wfMsg('sf_createclass_listofvalues') . '?';
-		$text =<<<END
+		$cp = SpecialPage::getPage( 'CreateProperty' );
+		$creation_links[] = $sk->makeKnownLinkObj( $cp->getTitle(), $cp->getDescription() );
+		$ct = SpecialPage::getPage( 'CreateTemplate' );
+		$creation_links[] = $sk->makeKnownLinkObj( $ct->getTitle(), $ct->getDescription() );
+		$cf = SpecialPage::getPage( 'CreateForm' );
+		$creation_links[] = $sk->makeKnownLinkObj( $cf->getTitle(), $cf->getDescription() );
+		$cc = SpecialPage::getPage( 'CreateCategory' );
+		$creation_links[] = $sk->makeKnownLinkObj( $cc->getTitle(), $cc->getDescription() );
+		$create_class_docu = wfMsg( 'sf_createclass_docu', $wgLang->listToText( $creation_links ) );
+		$leave_field_blank = wfMsg( 'sf_createclass_leavefieldblank' );
+		$form_name_label = wfMsg( 'sf_createform_nameinput' );
+		$template_name_label = wfMsg( 'sf_createtemplate_namelabel' );
+		$category_name_label = wfMsg( 'sf_createcategory_name' );
+		$property_name_label = wfMsg( 'sf_createproperty_propname' );
+		$field_name_label = wfMsg( 'sf_createtemplate_fieldname' );
+		$type_label = wfMsg( 'sf_createproperty_proptype' );
+		$allowed_values_label = wfMsg( 'sf_createclass_allowedvalues' ) . wfMsg( 'colon-separator' );
+		$list_of_values_label = wfMsg( 'sf_createclass_listofvalues' ) . '?';
+		$text = <<<END
 	<form action="" method="post">
 	<p>$create_class_docu</p>
 	<p>$leave_field_blank</p>
@@ -150,8 +150,8 @@ class SFCreateClass extends SpecialPage {
 	</tr>
 
 END;
-		for ($i = 1; $i <= 25; $i++) {
-			$text .=<<<END
+		for ( $i = 1; $i <= 25; $i++ ) {
+			$text .= <<<END
 	<tr>
 	<td>$i. <input type="text" size="25" name="property_name_$i" /></td>
 	<td><input type="text" size="25" name="field_name_$i" /></td>
@@ -159,10 +159,10 @@ END;
 	<select id="property_dropdown_$i" name="property_type_$i">
 
 END;
-			foreach ($datatype_labels as $label) {
+			foreach ( $datatype_labels as $label ) {
 				$text .= "	<option>$label</option>\n";
 			}
-			$text .=<<<END
+			$text .= <<<END
 	</select>
 	</td>
 	<td><input type="text" size="25" name="allowed_values_$i" /></td>
@@ -170,7 +170,7 @@ END;
 
 END;
 		}
-		$text .=<<<END
+		$text .= <<<END
 	</tr>
 	</table>
 	<br />
@@ -186,7 +186,7 @@ END;
 			'type' => 'text/css',
 			'media' => "screen",
 			'href' => $sfgScriptPath . "/skins/SF_main.css"
-		));
-		$wgOut->addHTML($text);
+		) );
+		$wgOut->addHTML( $text );
 	}
 }
