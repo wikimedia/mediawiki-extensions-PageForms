8706a7ec80e06eb0d8f2de41013a83b49a5e5c5d
Yaron Koren <yaron@users.mediawiki.org>
2010-08-23 12:47:30 +0000

    Overhaul - replaced YUI autocompletion code with Sanyam Goyal's jQuery autocompletion code

--- libs/SemanticForms.js
+++ libs/SemanticForms.js
@@ -1,152 +1,195 @@
-/*
+/**
  * sf_autocomplete()
  *
- * Creates an autocompletion widget, using the Yahoo! UI (YUI) library,
- * for the specified form input, using the specified values and delimiter
- * (in the case that it's a multiple-values autocompletion)
+ * Javascript utitilies for the Semantic Forms extension, mostly related to
+ * jQuery autocompletion.
+ *
+ * @author Sanyam Goyal
+ * @author Yaron Koren
  */
+
+function isEmpty(obj) {
+    for(var i in obj) {
+        return false;
+    }
+    return true;
+}
+
 function sf_autocomplete(input_name, container_name, values, api_url, data_type, delimiter, data_source) {
-    // Instantiate JS Function DataSource - the type depends on whether
-    // it gets autocompletion values locallly or remotely (through Ajax);
-    // which in turn is set depending on whether the 'values' or the
-    // 'data_type' and 'api_url' parameters are set
+    var myServer = api_url;
+    jQuery.noConflict();
+
+/* extending jquery functions for custom highlighting */
+     jQuery.ui.autocomplete.prototype._renderItem = function( ul, item) {
+
+          var re = new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + this.term.replace(/([\^\$\(\)\[\]\{\}\*\.\+\?\|\\])/gi, "\\$1") + ")(?![^<>]*>)(?![^&;]+;)", "gi");
+          var loc = item.label.search(re);
+	  if (loc >= 0) {
+          	var t = item.label.substr(0, loc) + '<strong>' + item.label.substr(loc, this.term.length) + '</strong>' + item.label.substr(loc + this.term.length);
+	} else {
+		var t = item.label;
+	}
+          return jQuery( "<li></li>" )
+              .data( "item.autocomplete", item )
+              .append( " <a>" + t + "</a>" )
+              .appendTo( ul );
+      };
+
+/* extending jquery functions  */
+      jQuery.extend( jQuery.ui.autocomplete, {	
+	filter: function(array, term) {
+		var matcher = new RegExp("\\b"+ jQuery.ui.autocomplete.escapeRegex(term), "i" );
+		return jQuery.grep( array, function(value) {
+			return matcher.test( value.label || value.value || value );
+		});
+	}
+    });
+
+
     if (values != null) {
-        this.oACDS = new YAHOO.widget.DS_JSFunction(autocompleteFunctionGenerator(values));
-        this.oACDS.maxCacheEntries = 0;
-        this.oAutoComp = new YAHOO.widget.AutoComplete(input_name, container_name, this.oACDS);
+            
+   /* delimiter != '' means multiple autocomplete */
+
+        if (delimiter != null) {
+
+            jQuery(document).ready(function(){
+                function split(val) {
+                    return val.split(delimiter+" ");
+                }
+		function extractLast(term) {
+			return split(term).pop();
+		}
+
+		jQuery("#" + input_name).autocomplete({
+			minLength: 0,
+			source: function(request, response) {
+
+				response(jQuery.ui.autocomplete.filter(values, extractLast(request.term)));
+			},
+			focus: function() {
+				// prevent value inserted on focus
+				return false;
+			},
+			select: function(event, ui) {
+				var terms = split( this.value );
+				// remove the current input
+				terms.pop();
+				// add the selected item
+				terms.push( ui.item.value );
+				// add placeholder to get the comma-and-space at the end
+				terms.push("");
+				this.value = terms.join(delimiter+" ");
+				return false;
+			}
+		});              
+            } );
+            
+        } else{
+            jQuery(document).ready(function(){
+                jQuery("#" + input_name).autocomplete({
+			source:values
+		});
+            } ) ;
+        }
     } else {
-        var myServer = api_url;
-        var mySchema = ["sfautocomplete", "title"];
-        this.oACDS = new YAHOO.widget.DS_XHR(myServer, mySchema);
-        this.oACDS.scriptQueryParam = "substr";
+                     
         if (data_type == 'property')
-            this.oACDS.scriptQueryAppend = "action=sfautocomplete&format=json&property=" + data_source;
+            myServer += "?action=sfautocomplete&format=json&property=" + data_source;
         else if (data_type == 'relation')
-            this.oACDS.scriptQueryAppend = "action=sfautocomplete&format=json&relation=" + data_source;
+            myServer += "?action=sfautocomplete&format=json&relation=" + data_source;
         else if (data_type == 'attribute')
-            this.oACDS.scriptQueryAppend = "action=sfautocomplete&format=json&attribute=" + data_source;
+            myServer += "?action=sfautocomplete&format=json&attribute=" + data_source;
         else if (data_type == 'category')
-            this.oACDS.scriptQueryAppend = "action=sfautocomplete&format=json&category=" + data_source;
-        else if (data_type == 'concept')
-            this.oACDS.scriptQueryAppend = "action=sfautocomplete&format=json&concept=" + data_source;
+            myServer += "?action=sfautocomplete&format=json&category=" + data_source;
         else if (data_type == 'namespace')
-            this.oACDS.scriptQueryAppend = "action=sfautocomplete&format=json&namespace=" + data_source;
+            myServer += "?action=sfautocomplete&format=json&namespace=" + data_source;
         else if (data_type == 'external_url')
-            this.oACDS.scriptQueryAppend = "action=sfautocomplete&format=json&external_url=" + data_source;
-        this.oACDS.responseType = YAHOO.widget.DS_XHR.TYPE_JSON;
-        this.oAutoComp = new YAHOO.widget.AutoComplete(input_name, container_name, this.oACDS);
-    }
+            myServer += "?action=sfautocomplete&format=json&external_url=" + data_source;                   
+       
+       if (delimiter != null) {
+            
+            jQuery(document).ready(function(){
+                function split(val) {
+			return val.split(delimiter + " ");
+		}
+		function extractLast(term) {
+			return split(term).pop();
+		}
+                        jQuery("#" + input_name).autocomplete({
+			source: function(request, response) {
+				jQuery.getJSON(myServer, {
+					substr: extractLast(request.term)
+				}, function( data ) {
+                            response(jQuery.map(data.sfautocomplete, function(item) {
+                                return {
+                                    value: item.title
+                                }
+                            }))
 
-    // Instantiate AutoComplete
-    this.oAutoComp.alwaysShowContainer = false;
-    this.oAutoComp.minQueryLength = 1;
-    this.oAutoComp.maxResultsDisplayed = 20;
-    this.oAutoComp.animHoriz = false;
-    this.oAutoComp.animVert = false;
-    this.oAutoComp.useIFrame = true;
-    if (delimiter != '') {
-        this.oAutoComp.delimChar = delimiter;
-    }
-    // don't set IFrame, which is meant to improve formatting on Internet
-    // Explorer - currently, it only messes up the formatting
-    //this.oAutoComp.useIFrame = true;
-    // This function returns markup that bolds the original query
-    this.oAutoComp.formatResult = function(aResultItem, sQuery) {
-        var sKey = aResultItem[0]; // the entire result key
-        var sKeyIndex = sKey.toLowerCase().indexOf(sQuery.toLowerCase());
-        // if it's not the very beginning, make sure the query string
-        // comes after a space
-        if (sKeyIndex > 0) {
-            sKeyIndex = sKey.toLowerCase().indexOf(' ' + sQuery.toLowerCase()) + 1;
+					});
+			},
+			search: function() {
+				// custom minLength
+				var term = extractLast(this.value);
+				if (term.length < 1) {
+					return false;
+				}
+			},
+			focus: function() {
+				// prevent value inserted on focus
+				return false;
+			},
+			select: function(event, ui) {
+				var terms = split( this.value );
+				// remove the current input
+				terms.pop();
+				// add the selected item
+				terms.push( ui.item.value );
+				// add placeholder to get the comma-and-space at the end
+				terms.push("");
+				this.value = terms.join(delimiter+" ");
+				return false;
+			}
+		});	
+
+              
+            } );
+        } else {
+		jQuery(document).ready(function(){
+		jQuery("#" + input_name).autocomplete({
+			minLength: 1,
+			source: function(request, response) {
+				jQuery.ajax({
+					url: myServer,
+					dataType: "json",
+					data: { 
+                                            substr:request.term
+                                            
+                                        },
+					success: function( data ) {
+						response(jQuery.map(data.sfautocomplete, function(item) {
+						return {
+							value: item.title
+						}
+					}))
+					}                                         
+				});
+			},                      
+			open: function() {
+				jQuery(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+			},
+			close: function() {
+				jQuery(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+			}
+		});
+
+            } );
         }
-        var sKeyBefore = sKey.substr(0, sKeyIndex);
-        var sKeyQuery = sKey.substr(sKeyIndex, sQuery.length); // the query itself
-        var sKeyAfter = sKey.substr(sKeyIndex + sQuery.length); // the rest of the result
-
-        var aMarkup = sKeyBefore + 
-            "<span style='font-weight:bold'>" +
-            sKeyQuery + 
-            "</span>" +
-            sKeyAfter;
-        return aMarkup;
-    };
-
-    // Show custom message if no results found
-    this.myOnDataReturn = function(sType, aArgs) {
-        var oAutoComp = aArgs[0];
-        var sQuery = aArgs[1];
-        var aResults = aArgs[2];
-
-        if(aResults.length == 0) {
-            oAutoComp.setBody("<div>No matching results</div>");
-        }
-    };
-
-    var itemSelectHandler = function (oAutoComp, listItem, dataArray) {
     }
-
-    // neither of these events are necessary to subscribe to yet
-    //this.oAutoComp.itemSelectEvent.subscribe(itemSelectHandler);
-    //this.oAutoComp.dataReturnEvent.subscribe(this.myOnDataReturn);
-
-    // Preload content in the container
-    //this.oAutoComp.sendQuery("");
 };
 
 /*
- * Helper function - returns a function that returns, for a given query
- * string, an array of values that match it
- */
-function autocompleteFunctionGenerator(values_list) {
-    return function (sQuery) {
-        var primaryResults = [];
-        var secondaryResults = [];
-        if (sQuery && sQuery.length > 0) {
-            // in some cases, decodeURI() doesn't handle certain characters
-            // correctly - replace them manually
-            query_str = decodeURI(sQuery).replace(/(%3A)/g, ":").toLowerCase();
-            query_str = query_str.replace(/(%24)/g, "$");
-            query_str = query_str.replace(/(%2f)/g, "/");
-            for (var i = 0; i < values_list.length; i++) {
-                subarray = values_list[i];
-                // workaround for strange IE bug
-                var name;
-                if (subarray.length > 1) {
-                    name = subarray;
-                } else {
-                    name = subarray[0];
-                }
-                name_str = name.toLowerCase();
-                var sKeyIndex = name_str.indexOf(query_str);
-                if (sKeyIndex == 0) {
-                    primaryResults.push(subarray);
-                } else {
-                    var index2 = name_str.indexOf(" " + query_str);
-                    if (index2 >= 0) {
-                        secondaryResults.push(subarray);
-                    }
-                }
-            }
-            var aResults = primaryResults.concat(secondaryResults);
-            // return no values if there's only one value, and the query
-            // exactly matches it
-            if (aResults.length == 1 && query_str.length == aResults[0][0].length) {
-                return [];
-            } else {
-                return aResults;
-            }
-        }
-        // Empty queries return all values
-        else {
-            return values_list;
-        }
-    }
-}
-
-/*
- * The following two functions are not related to YUI or autocompletion -
- * there just wasn't a better place to put them, and it seemed like overkill
- * to create a separate file just for them
+ * Functions for handling 'show on select' and 'show on check'
  */
 
 // show the relevant div if any one of the relevant options are passed in
@@ -176,3 +219,77 @@ function showIfChecked(checkbox_inputs, div_id) {
 	}
 	the_div.style.display = 'none';
 }
+
+
+/* extending jquery functions  */
+    
+
+
+
+(function(jQuery) {
+     
+	jQuery.widget("ui.combobox", {
+		_create: function() {
+			var self = this;
+			var select = this.element.hide();
+			var name= select[0].name;
+			var id = select[0].id;
+			var curval = select[0].options[0].value;
+			var input = jQuery("<input id=\"" + id + "\" type=\"text\" name=\" " + name + " \" value=\"" + curval + "\">")
+				.insertAfter(select)
+				.autocomplete({
+					source: function(request, response) {
+						var matcher = new RegExp("\\b"+request.term, "i");
+						response(select.children("option").map(function() {
+							var text = jQuery(this).text();
+							if (this.value && (!request.term || matcher.test(text))) {
+								return {
+									id: this.value,
+									label: text.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + jQuery.ui.autocomplete.escapeRegex(request.term) + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>"),
+									value: text
+								};
+							}
+						}));
+					},
+					delay: 0,
+					change: function(event, ui) {
+						if (!ui.item) {
+							// remove invalid value, as it didn't match anything
+							//jQuery(this).val("");
+							return false;
+						}
+						select.val(ui.item.id);
+						self._trigger("selected", event, {
+							item: select.find("[value='" + ui.item.id + "']")
+						});
+
+					},
+					minLength: 0
+				})
+			.addClass("ui-widget ui-widget-content ui-corner-left");
+		jQuery("<button type=\"button\">&nbsp;</button>")
+			.attr("tabIndex", -1)
+			.attr("title", "Show All Items")
+			.insertAfter(input)
+			.button({
+				icons: {
+					primary: "ui-icon-triangle-1-s"
+				},
+				text: false
+			}).removeClass("ui-corner-all")
+			.addClass("ui-corner-right ui-button-icon")
+			.click(function() {
+				// close if already visible
+				if (input.autocomplete("widget").is(":visible")) {
+					input.autocomplete("close");
+					return;
+				}
+				// pass empty string as value to search for, displaying all results
+				input.autocomplete("search", "");
+				input.focus();
+			});
+		}
+	});
+
+})(jQuery);
+
