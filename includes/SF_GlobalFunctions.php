f4032753fd46e68a0d8bd2f1ee9ea305463cc0ac
Yaron Koren <yaron@users.mediawiki.org>
2010-11-29 21:13:59 +0000

    New version: 2.0.6

--- includes/SF_GlobalFunctions.php
+++ includes/SF_GlobalFunctions.php
@@ -7,7 +7,7 @@
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define( 'SF_VERSION', '2.0.5' );
+define( 'SF_VERSION', '2.0.6' );
 
 $wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'specialpage'][] = array(
 	'path' => __FILE__,
