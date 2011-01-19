88014c27d14f763c706902434d7f29a6d5b8028a
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-01-19 18:04:30 +0000

    Fixed bc issue

--- includes/SF_FormEditTab.php
+++ includes/SF_FormEditTab.php
@@ -22,7 +22,7 @@ class SFFormEditTab {
 			( $obj->mTitle->getNamespace() == NS_SPECIAL ) ) {
 			return true;
 		}
-		$title = $obj->getTitle();
+		$title = $obj->mTitle; // getTitle method doesn't work for MW 1.15.x
 		$form_names = SFFormLinker::getDefaultFormsForPage( $title );
 		if ( count( $form_names ) == 0 ) {
 			return true;
