4f93b590b7e98f5e3cfb7ba7dc9bc2fd74d9c933
Yaron Koren <yaron@users.mediawiki.org>
2011-03-02 00:21:59 +0000

    Placeholder for backward compatibility with pre-version 2.0 SF installations - will eventually be removed again

---
+++ includes/SF_Settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * This file exists here for backward compatibility with installations of
+ * Semantic Forms before version 2.0.
+ */
+
+require_once dirname( __FILE__ ) . '/../SemanticForms.php';
