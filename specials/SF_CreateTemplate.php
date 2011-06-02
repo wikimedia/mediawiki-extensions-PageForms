ad04fd9bf928d3b454eb48ee8862819f1342b384
Yaron Koren <yaron@users.mediawiki.org>
2011-06-02 21:03:56 +0000

    Added support for SMW 1.6

--- specials/SF_CreateTemplate.php
+++ specials/SF_CreateTemplate.php
@@ -37,18 +37,29 @@ class SFCreateTemplate extends SpecialPage {
 		$options = new SMWRequestOptions();
 		$options->limit = 500;
 		$used_properties = smwfGetStore()->getPropertiesSpecial( $options );
-		
 		foreach ( $used_properties as $property ) {
-			$all_properties[] = $property[0]->getWikiValue();
-		}
-		
-		$unused_properties = smwfGetStore()->getUnusedPropertiesSpecial( $options );
-		
-		foreach ( $unused_properties as $property ) {
-			$all_properties[] = $property->getWikiValue();
+			if ( $property[0] instanceof SMWDIProperty ) {
+				// SMW 1.6+
+				$propName = $property[0]->getKey();
+				if ( $propName{0} != '_' ) {
+					$all_properties[] = str_replace( '_', ' ', $propName );
+				}
+			} else {
+				$all_properties[] = $property[0]->getWikiValue();
+			}
 		}
 
-		// sort properties list alphabetically
+		$unused_properties = smwfGetStore()->getUnusedPropertiesSpecial( $options );
+		foreach ( $unused_properties as $property ) {
+			if ( $property instanceof SMWDIProperty ) {
+				// SMW 1.6+
+				$all_properties[] = str_replace( '_' , ' ', $property->getKey() );
+			} else {
+				$all_properties[] = $property->getWikiValue();
+			}
+		}
+
+		// Sort properties list alphabetically.
 		sort( $all_properties );
 		return $all_properties;
 	}
@@ -184,7 +195,7 @@ END;
 
 		// Set 'title' field, in case there's no URL niceness
 		$ct = Title::makeTitleSafe( NS_SPECIAL, 'CreateTemplate' );
-		$text .= "\t" . Html::Hidden( 'title', SFUtils::titleURLString( $ct ) ) . "\n";
+		$text .= "\t" . Html::hidden( 'title', SFUtils::titleURLString( $ct ) ) . "\n";
 		$text .= "\t<p id=\"template_name_p\">" . wfMsg( 'sf_createtemplate_namelabel' ) . ' <input size="25" id="template_name" name="template_name" /></p>' . "\n";
 		$text .= "\t<p>" . wfMsg( 'sf_createtemplate_categorylabel' ) . ' <input size="25" name="category" /></p>' . "\n";
 		$text .= "\t<fieldset>\n";
