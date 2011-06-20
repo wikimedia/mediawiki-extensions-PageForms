7ef7962e32fcdfb6717be8491bc40e568ca11da1
Yaron Koren <yaron@users.mediawiki.org>
2011-06-20 03:07:59 +0000

    New file, supplied by "Van de Bugger"

---
+++ languages/SF_LanguageRu.php
@@ -0,0 +1,13 @@
+<?php
+
+class SF_LanguageRu extends SF_Language {
+
+	var $m_SpecialProperties = array(
+	);
+	
+	var $m_Namespaces = array(
+		SF_NS_FORM           => 'Фо�.ма',
+		SF_NS_FORM_TALK      => '�.бс�.ждение_�.о�.м�.'
+	);
+
+}
