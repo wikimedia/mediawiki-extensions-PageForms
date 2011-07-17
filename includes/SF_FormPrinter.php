c97dd478faf8a967a67492b932e8bdf49bea766e
Yaron Koren <yaron@users.mediawiki.org>
2011-07-17 22:45:30 +0000

    Partial revert of r92410 - removed HTML-escaping; this is the wrong place for it

--- includes/SF_FormPrinter.php
+++ includes/SF_FormPrinter.php
@@ -807,7 +807,7 @@ END;
 							$field_query_val = $template_instance_query_values[$field_name];
 						}
 						// Escaping of quotes is necessary.
-						$field_query_val = htmlentities( $field_query_val );
+						$field_query_val = str_replace( '"', '&quot;', $field_query_val );
 						if ( $form_submitted || ( ! empty( $field_query_val ) && ! is_array( $field_query_val ) ) ) {
 							$cur_value = $field_query_val;
 						}
