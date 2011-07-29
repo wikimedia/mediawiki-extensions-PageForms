79dc254392719b014a4facce4cec934616b92778
Yaron Koren <yaron@users.mediawiki.org>
2011-07-29 20:31:05 +0000

    New version: 2.2.1

--- SemanticForms.php
+++ SemanticForms.php
@@ -36,7 +36,7 @@
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define( 'SF_VERSION', '2.2' );
+define( 'SF_VERSION', '2.2.1' );
 
 $wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'specialpage'][] = array(
 	'path' => __FILE__,
