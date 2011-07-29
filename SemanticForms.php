dff79f4ed7821c7647287fc8f0749c20276595ce
Ankit Garg <ankitgarg833@users.mediawiki.org>
2011-07-29 16:17:30 +0000

    adding hook code for PageSchema . getHtmlTextForPS

--- SemanticForms.php
+++ SemanticForms.php
@@ -92,6 +92,7 @@ $wgHooks['PageSchemasGetObject'][] = 'SFUtils::createPageSchemasObject' ; //Hook
 $wgHooks['PageSchemasGeneratePages'][] = 'SFUtils::generatePages' ; //Hook for  creating Pages
 $wgHooks['PSParseFieldElements'][] = 'SFUtils::parseFieldElements' ; //Hook for  creating Pages
 $wgHooks['PageSchemasGetPageList'][] = 'SFUtils::getPageList' ; //Hook for  creating Pages
+$wgHooks['getHtmlTextForFieldInputs'][] = 'SFUtils::getHtmlTextForPS' ; //Hook for  retuning html text to PS schema
 
 
 $wgAPIModules['sfautocomplete'] = 'SFAutocompleteAPI';
