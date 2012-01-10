27bcb71e38289cdb3fb70e77948651c871ff0161
Yaron Koren <yaron@users.mediawiki.org>
2012-01-10 22:28:30 +0000

    Moved function into class, replaced some hardcoded HTML with calls to Xml class

--- specials/SF_CreateCategory.php
+++ specials/SF_CreateCategory.php
@@ -20,11 +20,6 @@ class SFCreateCategory extends SpecialPage {
 		parent::__construct( 'CreateCategory' );
 	}
 
-	function execute( $query ) {
-		$this->setHeaders();
-		doSpecialCreateCategory();
-	}
-
 	static function createCategoryText( $default_form, $category_name, $parent_category ) {
 
 		if ( $default_form === '' ) {
@@ -43,86 +38,95 @@ class SFCreateCategory extends SpecialPage {
 		}
 		return $text;
 	}
-}
 
-function doSpecialCreateCategory() {
-	global $wgOut, $wgRequest, $wgUser, $sfgScriptPath;
+	function execute( $query ) {
+		global $wgOut, $wgRequest, $wgUser, $sfgScriptPath;
 
-	# cycle through the query values, setting the appropriate local variables
-	$category_name = $wgRequest->getVal( 'category_name' );
-	$default_form = $wgRequest->getVal( 'default_form' );
-	$parent_category = $wgRequest->getVal( 'parent_category' );
+		$this->setHeaders();
 
-	$category_name_error_str = '';
-	$save_page = $wgRequest->getCheck( 'wpSave' );
-	$preview_page = $wgRequest->getCheck( 'wpPreview' );
-	if ( $save_page || $preview_page ) {
-		# validate category name
-		if ( $category_name === '' ) {
-			$category_name_error_str = wfMsg( 'sf_blank_error' );
-		} else {
-			# redirect to wiki interface
-			$wgOut->setArticleBodyOnly( true );
-			$title = Title::makeTitleSafe( NS_CATEGORY, $category_name );
-			$full_text = SFCreateCategory::createCategoryText( $default_form, $category_name, $parent_category );
-			$text = SFUtils::printRedirectForm( $title, $full_text, "", $save_page, $preview_page, false, false, false, null, null );
-			$wgOut->addHTML( $text );
-			return;
+		// Cycle through the query values, setting the appropriate
+		// local variables.
+		$category_name = $wgRequest->getVal( 'category_name' );
+		$default_form = $wgRequest->getVal( 'default_form' );
+		$parent_category = $wgRequest->getVal( 'parent_category' );
+
+		$category_name_error_str = null;
+		$save_page = $wgRequest->getCheck( 'wpSave' );
+		$preview_page = $wgRequest->getCheck( 'wpPreview' );
+		if ( $save_page || $preview_page ) {
+			// Validate category name
+			if ( $category_name === '' ) {
+				$category_name_error_str = wfMsg( 'sf_blank_error' );
+			} else {
+				// Redirect to wiki interface
+				$wgOut->setArticleBodyOnly( true );
+				$title = Title::makeTitleSafe( NS_CATEGORY, $category_name );
+				$full_text = SFCreateCategory::createCategoryText( $default_form, $category_name, $parent_category );
+				$text = SFUtils::printRedirectForm( $title, $full_text, "", $save_page, $preview_page, false, false, false, null, null );
+				$wgOut->addHTML( $text );
+				return;
+			}
 		}
-	}
 
-	$all_forms = SFUtils::getAllForms();
+		$all_forms = SFUtils::getAllForms();
 
-	// set 'title' as hidden field, in case there's no URL niceness
-	global $wgContLang;
-	$mw_namespace_labels = $wgContLang->getNamespaces();
-	$special_namespace = $mw_namespace_labels[NS_SPECIAL];
-	$name_label = wfMsg( 'sf_createcategory_name' );
-	$form_label = wfMsg( 'sf_createcategory_defaultform' );
-	$text = <<<END
+		// Set 'title' as hidden field, in case there's no URL niceness
+		global $wgContLang;
+		$mw_namespace_labels = $wgContLang->getNamespaces();
+		$special_namespace = $mw_namespace_labels[NS_SPECIAL];
+		$text = <<<END
 	<form action="" method="get">
 
 END;
-	$text .= SFFormUtils::hiddenFieldHTML( 'title', "$special_namespace:CreateCategory" );
-	$text .= <<<END
-	<p>$name_label <input size="25" name="category_name" value="" />
-	<span style="color: red;">$category_name_error_str</span>
-	$form_label
-	<select id="form_dropdown" name="default_form">
-	<option></option>
+		$text .= SFFormUtils::hiddenFieldHTML( 'title', "$special_namespace:CreateCategory" );
+		$firstRow = wfMsg( 'sf_createcategory_name' ) . ' ' .
+			Xml::element( 'input',
+				array(
+					'size' => 25,
+					'name' => 'category_name',
+				), null ) . "\n";
+		if ( !is_null( $category_name_error_str ) ) {
+			$firstRow .= Xml::element( 'span',
+				array( 'style' => 'color: red;' ),
+				$category_name_error_str ) . "\n";
+		}
+		$firstRow .= "\t" . wfMsg( 'sf_createcategory_defaultform' ) . "\n";
+		$formSelector = "\t" . Xml::element( 'option', null, null ). "\n";
+		foreach ( $all_forms as $form ) {
+			$formSelector .= "\t" . Xml::element( 'option', null, $form ) . "\n";
+		}
 
-END;
-	foreach ( $all_forms as $form ) {
-		$text .= "\t" . Xml::element( 'option', null, $form ) . "\n";
-	}
-
-	$subcategory_label = wfMsg( 'sf_createcategory_makesubcategory' );
-	$categories = SFUtils::getCategoriesForPage();
-	$sk = $wgUser->getSkin();
-	$create_form_link = SFUtils::linkForSpecialPage( $sk, 'CreateForm' );
-	$text .= <<<END
-	</select>
+		$firstRow .= Xml::tags( 'select',
+			array( 'id' => 'form_dropdown', 'name' => 'default_form' ),
+			$formSelector );
+		$text .= Xml::tags( 'p', null, $firstRow );
+		$subcategory_label = wfMsg( 'sf_createcategory_makesubcategory' );
+		$text .= <<<END
 	<p>$subcategory_label
 	<select id="category_dropdown" name="parent_category">
 	<option></option>
 
 END;
-	foreach ( $categories as $category ) {
-		$category = str_replace( '_', ' ', $category );
-		$text .= "\t" . Xml::element( 'option', null, $category ) . "\n";
-	}
-	$text .= "\t</select>\n";
-	$editButtonsText = "\t" . Xml::element( 'input', array( 'type' => 'submit', 'id' => 'wpSave', 'name' => 'wpSave', 'value' => wfMsg( 'savearticle' ) ) ) . "\n";
-	$editButtonsText .= "\t" . Xml::element( 'input', array( 'type' => 'submit', 'id' => 'wpPreview', 'name' => 'wpPreview', 'value' => wfMsg( 'preview' ) ) ) . "\n";
-	$text .= "\t" . Xml::tags( 'div', array( 'class' => 'editButtons' ), $editButtonsText ) . "\n";
-	$text .= <<<END
+		$categories = SFUtils::getCategoriesForPage();
+		foreach ( $categories as $category ) {
+			$category = str_replace( '_', ' ', $category );
+			$text .= "\t" . Xml::element( 'option', null, $category ) . "\n";
+		}
+		$text .= "\t</select>\n";
+		$editButtonsText = "\t" . Xml::element( 'input', array( 'type' => 'submit', 'id' => 'wpSave', 'name' => 'wpSave', 'value' => wfMsg( 'savearticle' ) ) ) . "\n";
+		$editButtonsText .= "\t" . Xml::element( 'input', array( 'type' => 'submit', 'id' => 'wpPreview', 'name' => 'wpPreview', 'value' => wfMsg( 'preview' ) ) ) . "\n";
+		$text .= "\t" . Xml::tags( 'div', array( 'class' => 'editButtons' ), $editButtonsText ) . "\n";
+		$text .= <<<END
 	<br /><hr /<br />
 
 END;
 
-	$text .= "	" . Xml::tags( 'p', null, $create_form_link . '.' ) . "\n";
-	$text .= "	</form>\n";
+		$sk = $wgUser->getSkin();
+		$create_form_link = SFUtils::linkForSpecialPage( $sk, 'CreateForm' );
+		$text .= "\t" . Xml::tags( 'p', null, $create_form_link . '.' ) . "\n";
+		$text .= "\t</form>\n";
 
-	$wgOut->addExtensionStyle( $sfgScriptPath . "/skins/SemanticForms.css" );
-	$wgOut->addHTML( $text );
+		$wgOut->addExtensionStyle( $sfgScriptPath . "/skins/SemanticForms.css" );
+		$wgOut->addHTML( $text );
+	}
 }
