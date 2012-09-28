0f16ad627c3d33e5a4c1fb589f76371afb6b2bbd
yaron <yaron57@gmail.com>
2012-09-28 20:07:37 +0400

    Removed GlobalFunctions.php, moved its contents to SemanticForms.php
    Change-Id: I683871d70dc14d026bafad49fdcc71988e773c42

--- SemanticForms.php
+++ SemanticForms.php
@@ -278,8 +278,44 @@ if ( defined( 'MW_SUPPORTS_RESOURCE_MODULES' ) ) {
 // PHP fails to find relative includes at some level of inclusion:
 // $pathfix = $IP . $sfgScriptPath;
 
-// load global functions
-require_once( 'includes/SF_GlobalFunctions.php' );
+// Global functions
+
+/**
+ *  This is a delayed init that makes sure that MediaWiki is set up
+ *  properly before we add our stuff.
+ */
+function sffSetupExtension() {
+	// This global variable is needed so that other extensions can hook
+	// into it to add their own input types.
+	global $sfgFormPrinter;
+	$sfgFormPrinter = new StubObject( 'sfgFormPrinter', 'SFFormPrinter' );
+}
+
+/**
+ * Initialize a global language object for content language. This
+ * must happen early on, even before user language is known, to
+ * determine labels for additional namespaces. In contrast, messages
+ * can be initialised much later, when they are actually needed.
+ */
+function sffInitContentLanguage( $langcode ) {
+	global $sfgIP, $sfgContLang;
+
+	if ( !empty( $sfgContLang ) ) { return; }
+
+	$cont_lang_class = 'SF_Language' . str_replace( '-', '_', ucfirst( $langcode ) );
+	if ( file_exists( $sfgIP . '/languages/' . $cont_lang_class . '.php' ) ) {
+		include_once( $sfgIP . '/languages/' . $cont_lang_class . '.php' );
+	}
+
+	// fallback if language not supported
+	if ( !class_exists( $cont_lang_class ) ) {
+		include_once( $sfgIP . '/languages/SF_LanguageEn.php' );
+		$cont_lang_class = 'SF_LanguageEn';
+	}
+
+	$sfgContLang = new $cont_lang_class();
+}
+
 
 sffInitContentLanguage( $wgLanguageCode );
 
