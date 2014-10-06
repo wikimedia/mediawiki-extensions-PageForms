8bdcec379b064e81358513442affa3084bb4efe8
Ike Hecht <tosfos@yahoo.com>
2014-10-06 14:28:13 -0700

    Introduce sfBeforeFreeTextSubstitution hook, replacing the now deprecated sfModifyFreeTextField hook
    Change-Id: I7103b0d039b0efc141588dd60b77d936f011c51a

--- includes/SF_FormPrinter.php
+++ includes/SF_FormPrinter.php
@@ -1728,7 +1728,12 @@ END;
 			$data_text = str_replace( '!free_text!', '<onlyinclude>!free_text!</onlyinclude>', $data_text );
 		}
 
+		// The first hook here is deprecated. Use the second.
+		// Note: wfRunHooks can take a third argument which indicates a deprecated hook, but it
+		// expects a MediaWiki version, not an extension version.
 		wfRunHooks( 'sfModifyFreeTextField', array( &$free_text, $existing_page_content ) );
+		wfRunHooks( 'sfBeforeFreeTextSubstitution',
+			array( &$free_text, $existing_page_content, &$data_text ) );
 
 		// now that we have it, substitute free text into the form and page
 		$escaped_free_text = Sanitizer::safeEncodeAttribute( $free_text );
