3f4c59c46c21d18d8ce30a350ce005865cf649e4
Umherirrender <umherirrender_de.wp@web.de>
2016-12-23 21:50:12 +0100

    Activate grunt-banana-checker
    Change-Id: Ie816229af63cc97131628be6067c10c08b112a9c

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
-	// grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	grunt.initConfig( {
 		jshint: {
@@ -19,9 +19,9 @@ module.exports = function ( grunt ) {
 				'!libs/PF_maps.js'
 			]
 		},
-		// banana: {
-			// all: 'i18n/'
-		// },
+		banana: {
+			all: 'i18n/'
+		},
 		jsonlint: {
 			all: [
 				'**/*.json',
@@ -30,6 +30,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jsonlint' /* 'banana' */ ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
