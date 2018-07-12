4e4f96b5cbb56f445d9f206ccb60688f7281b95a
James D. Forrester <jforrester@wikimedia.org>
2018-07-12 10:41:25 -0700

    Replace use of deprecated OutputPage::showFileDeleteError()
    Change-Id: I5f7882e4f7b4e5393aa537e69dc6357b14b858f3

--- specials/PF_UploadWindow.php
+++ specials/PF_UploadWindow.php
@@ -578,7 +578,11 @@ END;
 		}
 		$success = $this->mUpload->unsaveUploadedFile();
 		if ( ! $success ) {
-			$this->getOutput()->showFileDeleteError( $this->mUpload->getTempPath() );
+			$this->getOutput()->showFatalError(
+				$this->msg( 'filedeleteerror' )
+					->params( $this->mUpload->getTempPath() )
+					->escaped()
+			);
 			return false;
 		} else {
 			return true;
