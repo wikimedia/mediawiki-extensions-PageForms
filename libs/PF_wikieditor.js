89d2e764e805233857df45b7ec2fc6594964569d
Cindy Cicalese <cindom@gmail.com>
2018-11-29 15:13:46 +0000

    Handle calls to removed WikiEditor.isSupported() function
    WikiEditor.isSupported() was removed in REL1_31. This patch
    maintains backward compatibility.
    
    Change-Id: I6dd178a350e8fae334e90fc002ad852b241c45f5

--- libs/PF_wikieditor.js
+++ libs/PF_wikieditor.js
@@ -15,7 +15,7 @@ window.ext.wikieditor = {
 
 				// load toolbar
 				mw.loader.using( ['jquery.wikiEditor.toolbar', 'jquery.wikiEditor.toolbar.config'] , function() {
-					if ( $.wikiEditor.isSupported( $.wikiEditor.modules.toolbar ) ) {
+					if ( typeof $.wikiEditor.isSupported !== 'function' || $.wikiEditor.isSupported( $.wikiEditor.modules.toolbar ) ) {
 						input.wikiEditor( 'addModule', $.wikiEditor.modules.toolbar.config.getDefaultConfig() );
 
 						// hide sig if required
@@ -32,7 +32,7 @@ window.ext.wikieditor = {
 
 				// load dialogs
 				mw.loader.using( ['jquery.wikiEditor.dialogs', 'jquery.wikiEditor.dialogs.config'] , function(){
-					if ( $.wikiEditor.isSupported( $.wikiEditor.modules.dialogs ) ) {
+					if ( typeof $.wikiEditor.isSupported !== 'function' || $.wikiEditor.isSupported( $.wikiEditor.modules.dialogs ) ) {
 						$.wikiEditor.modules.dialogs.config.replaceIcons( input );
 						input.wikiEditor( 'addModule', $.wikiEditor.modules.dialogs.config.getDefaultConfig() );
 
