dafc06f5d89922af20363294c2d1c6d0ae16ec63
Yaron Koren <yaron57@gmail.com>
2019-01-24 16:26:46 +0000

    Get form JS to activate on form preview page
    Change-Id: I1f8bfbe8b69e2b5eb928d9a5a40215874698d2f9

--- includes/PF_Hooks.php
+++ includes/PF_Hooks.php
@@ -324,8 +324,9 @@ class PFHooks {
 			return true;
 		}
 
+		// The "pfForm" ID is there so the form JS will be activated.
 		$editpage->previewTextAfterContent .= Html::element( 'h2', null, wfMessage( 'pf-preview-header' )->text() ) . "\n" .
-			'<div class="previewnote" style="font-weight: bold">' . $wgOut->parse( wfMessage( 'pf-preview-note' )->text() ) . "</div>\n<hr />\n";
+			'<div id="pfForm" class="previewnote" style="font-weight: bold">' . $wgOut->parse( wfMessage( 'pf-preview-note' )->text() ) . "</div>\n<hr />\n";
 
 		$form_definition = StringUtils::delimiterReplace( '<noinclude>', '</noinclude>', '', $editpage->textbox1 );
 		list( $form_text, $data_text, $form_page_title, $generated_page_name ) =
