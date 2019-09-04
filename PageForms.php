71f42d6b23a949567b1a35ff3c3457f3dcd95a4e
Yaron Koren <yaron57@gmail.com>
2019-09-04 14:56:02 +0000

    Version 4.6
    Change-Id: I1b0fce04457e00dbd12f05f2aa5f56f6569c440b

--- PageForms.php
+++ PageForms.php
@@ -69,7 +69,7 @@ if ( defined( 'PF_VERSION' ) ) {
 	return 1;
 }
 
-define( 'PF_VERSION', '4.5.1' );
+define( 'PF_VERSION', '4.6' );
 
 $GLOBALS['wgExtensionCredits']['specialpage'][] = array(
 	'path' => __FILE__,
