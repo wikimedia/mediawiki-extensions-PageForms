47af1201813e080ec8bd840feef8f96e5fc3d81d
Yaron Koren <yaron57@gmail.com>
2020-01-17 19:15:50 +0000

    Add "max height" param for textarea with VE
    Change-Id: I52d5b3937d59723ab378d95b12fbd937b15e3823

--- includes/forminputs/PF_TextAreaInput.php
+++ includes/forminputs/PF_TextAreaInput.php
@@ -307,7 +307,20 @@ class PFTextAreaInput extends PFFormInput {
 		if ( $this->mEditor == 'visualeditor' ) {
 			$spanClass .= ' ve-area-wrapper';
 		}
-		$text = Html::rawElement( 'span', [ 'class' => $spanClass ], $text );
+		$spanAttrs = [ 'class' => $spanClass ];
+		if ( $this->mEditor == 'visualeditor' ) {
+			// VisualEditor, by default, autogrows with no limit -
+			// which is fine in a regular edit page, but not good
+			// in a form. So add a "max-height" value.
+			if ( array_key_exists( 'max height', $this->mOtherArgs ) ) {
+				$maxHeight = (int)$this->mOtherArgs['max height'];
+			} else {
+				$maxHeight = 400;
+			}
+			$spanAttrs['style'] = "max-height: {$maxHeight}px; overflow-y: scroll;";
+		}
+
+		$text = Html::rawElement( 'span', $spanAttrs, $text );
 
 		return $text;
 	}
