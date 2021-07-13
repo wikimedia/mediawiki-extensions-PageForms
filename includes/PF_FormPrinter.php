509bc8fd23cac8a9196ac1ff19934e11b648843e
Yaron Koren <yaron57@gmail.com>
2021-07-13 22:50:02 +0000

    Fix "wrong form" warning for file or category pages
    Change-Id: I7a8fa8d733f16d8bd3e2b9a8e3a769a0ee9ced93

--- includes/PF_FormPrinter.php
+++ includes/PF_FormPrinter.php
@@ -1843,7 +1843,8 @@ END;
 			$this->mPageTitle->exists() && $existing_page_content !== ''
 			&& !$source_page_matches_this_form ) {
 			$form_text = "\t" . '<div class="warningbox">' .
-				wfMessage( 'pf_formedit_formwarning', $page_name )->parse() .
+				// Prepend with a colon in case it's a file or category page.
+				wfMessage( 'pf_formedit_formwarning', ':' . $page_name )->parse() .
 				"</div>\n<br clear=\"both\" />\n" . $form_text;
 		}
 
