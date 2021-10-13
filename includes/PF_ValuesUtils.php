004e9670a88f3fcc6e1430561455a852076ce3ec
Youri <yvdbogert@archixl.nl>
2021-10-13 10:28:28 +0200

    Fixes a bug where sometimes the displaytitle property would not be null but empty, thus resulting in titles not being found through the autocomplete API
    Change-Id: I32f5b5ad4d6579a19ddf185a610dbe87d354f98f

--- includes/PF_ValuesUtils.php
+++ includes/PF_ValuesUtils.php
@@ -231,7 +231,7 @@ class PFValuesUtils {
 						]
 					];
 					if ( $substring != null ) {
-						$conditions[] = '(pp_displaytitle.pp_value IS NULL AND (' .
+						$conditions[] = '(pp_displaytitle.pp_value IS NULL OR pp_displaytitle.pp_value = \'\' AND (' .
 							self::getSQLConditionForAutocompleteInColumn( 'page_title', $substring ) .
 							')) OR ' .
 							self::getSQLConditionForAutocompleteInColumn( 'pp_displaytitle.pp_value', $substring ) .
