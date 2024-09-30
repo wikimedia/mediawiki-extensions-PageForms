wikimedia/mediawiki-extensions-PageForms

commit 17d6fdb53badaf1156b9628cfa884c408fa0b406

Add $wgPageFormsDelayReload setting
Change-Id: Ia5f4a0d7c04c98339e5057c6fabff95842630362

includes/PF_AutoeditAPI.php
@@ -329,7 +329,7 @@ class PFAutoeditAPI extends ApiBase {
 		$data = array_merge(
 			[
 				'wpTextbox1' => $targetContent,
-				'wpUnicodeCheck' => 'ℳ𝒲♥𝓊𝓃𝒾𝒸ℴ𝒹ℯ',
+				'wpUnicodeCheck' => 'â„³ð’²â™¥ð“Šð“ƒð’¾ð’¸â„´ð’¹â„¯',
 				'wpSummary' => '',
 				'wpStarttime' => wfTimestampNow(),
 				'wpEditToken' => isset( $this->mOptions[ 'token' ] ) ? $this->mOptions[ 'token' ] : $this->getUser()->getEditToken(),
@@ -406,6 +406,8 @@ class PFAutoeditAPI extends ApiBase {
 	}
 
 	protected function doStore( EditPage $editor ) {
+		global $wgPageFormsDelayReload;
+
 		$title = $editor->getTitle();
 
 		// If they used redlink=1 and the page exists, redirect to the main article and send notice
@@ -533,7 +535,11 @@ class PFAutoeditAPI extends ApiBase {
 					if ( $returntoPage->exists() && $reload ) {
 						$returntoPage->doPurge();
 					}
-					$redirect = $returnto->getFullURL();
+					if ( $wgPageFormsDelayReload ) {
+						$redirect = $returnto->getFullURL( [ 'forceReload' => 'true' ] );
+					} else {
+						$redirect = $returnto->getFullURL();
+					}
 				}
 
 				$this->getOutput()->redirect( $redirect );
@@ -568,7 +574,11 @@ class PFAutoeditAPI extends ApiBase {
 					if ( $returntoPage->exists() && $reload ) {
 						$returntoPage->doPurge();
 					}
-					$redirect = $returnto->getFullURL();
+					if ( $wgPageFormsDelayReload ) {
+						$redirect = $returnto->getFullURL( [ 'forceReload' => 'true' ] );
+					} else {
+						$redirect = $returnto->getFullURL();
+					}
 				}
 
 				$this->getOutput()->redirect( $redirect );

includes/PF_Hooks.php
@@ -128,7 +128,7 @@ class PFHooks {
 		global $wgPageFormsGridValues, $wgPageFormsGridParams;
 		global $wgPageFormsCalendarValues, $wgPageFormsCalendarParams, $wgPageFormsCalendarHTML;
 		global $wgPageFormsContLangYes, $wgPageFormsContLangNo, $wgPageFormsContLangMonths;
-		global $wgPageFormsHeightForMinimizingInstances;
+		global $wgPageFormsHeightForMinimizingInstances, $wgPageFormsDelayReload;
 		global $wgPageFormsShowOnSelect, $wgPageFormsScriptPath;
 		global $edgValues, $wgPageFormsEDSettings;
 		global $wgAmericanDates;
@@ -148,6 +148,7 @@ class PFHooks {
 		$vars['wgPageFormsContLangNo'] = $wgPageFormsContLangNo;
 		$vars['wgPageFormsContLangMonths'] = $wgPageFormsContLangMonths;
 		$vars['wgPageFormsHeightForMinimizingInstances'] = $wgPageFormsHeightForMinimizingInstances;
+		$vars['wgPageFormsDelayReload'] = $wgPageFormsDelayReload;
 		$vars['wgPageFormsShowOnSelect'] = $wgPageFormsShowOnSelect;
 		$vars['wgPageFormsScriptPath'] = $wgPageFormsScriptPath;
 		if ( method_exists( 'EDParserFunctions', 'getAllValues' ) ) {
@@ -389,6 +390,36 @@ class PFHooks {
 		$postEditKey = EditPage::POST_EDIT_COOKIE_KEY_PREFIX . $revisionRecord->getID();
 		$response = RequestContext::getMain()->getRequest()->response();
 		$response->setCookie( $postEditKey, 'saved', time() + EditPage::POST_EDIT_COOKIE_DURATION );
+	}
+
+	/**
+	 * Called by the BeforePageDisplay hook.
+	 *
+	 * Reload the page if "forceReload=true" exists in the URL query string.
+	 * This is a @hack done so that the $wgPageFormsDelayReload setting
+	 * (itself a hack) can take effect - in some cases, having a #formlink
+	 * call with "returnto=" and "reload" both set does not actually
+	 * refresh the queries on the original page in time, so we use this to
+	 * then reload the original page, which does seem to work. There may
+	 * well be a better solution for this, though.
+	 *
+	 * @param MediaWiki\Output\OutputPage $out
+	 * @param Skin $skin
+	 */
+	public static function handleForceReload( $out, Skin $skin ) {
+		global $wgRequest, $wgPageFormsScriptPath;
+
+		if ( $wgRequest->getVal( 'forceReload' ) !== 'true' ) {
+			return;
+		}
+
+		$out->clearHTML();
+		$loadingImage = Html::element( 'img', [ 'src' => "$wgPageFormsScriptPath/skins/loading.gif" ] );
+		$text = "\t" . Html::rawElement( 'p', [ 'style' => "position: absolute; left: 45%; top: 45%;" ], $loadingImage );
+		$reloadURL = $out->getTitle()->getFullURL();
+		$text .= Html::element( 'meta', [ 'http-equiv' => 'refresh', 'content' => "0; url=$reloadURL" ] );
+
+		$out->addHTML( $text );
 	}
 
 }

libs/PF_autoedit.js
@@ -1,5 +1,5 @@
 /**
- * Javascript handler for the autoedit parser function
+ * JavaScript handler for the #autoedit parser function.
  *
  * @author Stephan Gambke
  */
@@ -41,7 +41,13 @@
 				if ( result.status === 200 ) {
 
 					if ( reload ) {
-						window.location.reload();
+						if ( mw.config.get( 'wgPageFormsDelayReload' ) == true ) {
+							setTimeout( function() {
+								window.location.reload()
+							}, 500 );
+						} else {
+							window.location.reload();
+						}
 					}
 
 					$jresult.removeClass( 'autoedit-result-wait' ).addClass( 'autoedit-result-ok' );

commit a2778d437ca4fa2e4c07ec5dc8b83fec77aa9d79

Fix unicode problem introduced in 17d6fdb53bad
Copy-paste error, probably.

Change-Id: I36c23d9adbbc8cef3bbef9ffd644361bfd9ba112

includes/PF_AutoeditAPI.php
@@ -329,7 +329,7 @@ class PFAutoeditAPI extends ApiBase {
 		$data = array_merge(
 			[
 				'wpTextbox1' => $targetContent,
-				'wpUnicodeCheck' => 'â„³ð’²â™¥ð“Šð“ƒð’¾ð’¸â„´ð’¹â„¯',
+				'wpUnicodeCheck' => 'ℳ𝒲♥𝓊𝓃𝒾𝒸ℴ𝒹ℯ',
 				'wpSummary' => '',
 				'wpStarttime' => wfTimestampNow(),
 				'wpEditToken' => isset( $this->mOptions[ 'token' ] ) ? $this->mOptions[ 'token' ] : $this->getUser()->getEditToken(),