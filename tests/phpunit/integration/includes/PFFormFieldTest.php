fc80c3e36cbcf584c50f9e6fb67e9e243381d03b
Wandji Collins <collinschuwa@gmail.com>
2026-04-29 19:29:42 +0100

    Enhanced integration test for PFFormField::newFromFormFieldTag method
    Bug: T369822
    Change-Id: Ia22d45c11009775386e988cc404c421bb437afba

--- tests/phpunit/integration/includes/PFFormFieldTest.php
+++ tests/phpunit/integration/includes/PFFormFieldTest.php
@@ -16,6 +16,67 @@ class PFFormFieldTest extends MediaWikiIntegrationTestCase {
 		parent::setUp();
 		$this->pfTemplateField = $this->createMock( \PFTemplateField::class );
 		$this->pfFormField = \PFFormField::create( $this->pfTemplateField );
+		$this->setMwGlobals( [
+			'wgPageFormsDependentFields' => [],
+			'wgPageFormsEmbeddedTemplates' => [],
+			'wgPageFormsUseDisplayTitle' => false,
+		] );
+	}
+
+	/**
+	 * Create a real PFFormField from the same collaborators used by form parsing.
+	 *
+	 * @param string $templateName
+	 * @param PFTemplateField[] $templateFields
+	 * @param string[] $fieldTagComponents
+	 * @param string[] $templateTagOptions
+	 * @param bool $formIsDisabled
+	 * @param User|null $user
+	 * @return PFFormField
+	 */
+	private function newFormFieldFromTags(
+		$templateName,
+		array $templateFields,
+		array $fieldTagComponents,
+		array $templateTagOptions = [],
+		$formIsDisabled = false,
+		?\User $user = null
+	) {
+		$template = new \PFTemplate( $templateName, $templateFields );
+		$templateInForm = \PFTemplateInForm::create(
+			$templateName,
+			null,
+			in_array( 'multiple', $templateTagOptions ),
+			null,
+			[]
+		);
+		if ( in_array( 'strict', $templateTagOptions ) ) {
+			$strictParsing = new \ReflectionProperty( \PFTemplateInForm::class, 'mStrictParsing' );
+			$strictParsing->setValue( $templateInForm, true );
+		}
+
+		\PFUtils::getParser()->startExternalParse(
+			null,
+			\ParserOptions::newFromAnon(),
+			\Parser::OT_WIKI
+		);
+
+		return \PFFormField::newFromFormFieldTag(
+			$fieldTagComponents,
+			$template,
+			$templateInForm,
+			$formIsDisabled,
+			$user ?: $this->newUserStub()
+		);
+	}
+
+	private function newUserStub( $canEditRestrictedFields = true ): \User {
+		$user = $this->createMock( \User::class );
+		$user->method( 'isAllowed' )
+			->willReturnCallback( static function ( string $right ) use ( $canEditRestrictedFields ) {
+				return $right === 'editrestrictedfields' ? $canEditRestrictedFields : true;
+			} );
+		return $user;
 	}
 
 	/**
@@ -130,6 +191,7 @@ class PFFormFieldTest extends MediaWikiIntegrationTestCase {
 		// Should return from template field if mPossibleValues is null
 		$this->pfTemplateField->method( 'getPossibleValues' )->willReturn( [ 'value1' => 'label1' ] );
 		$actual = $this->pfFormField->getPossibleValues();
+		$this->assertNotNull( $actual );
 		$this->assertIsArray( $actual );
 	}
 
@@ -163,8 +225,180 @@ class PFFormFieldTest extends MediaWikiIntegrationTestCase {
 		$this->assertTrue( true );
 	}
 
+	/*
+	 *@covers \PFFormField::newFromFormFieldTag
+	 */
 	public function testNewFromFormFieldTag() {
-		$this->assertTrue( method_exists( '\PFFormField', 'newFromFormFieldTag' ) );
+		$templateField = \PFTemplateField::create( 'Status', 'Status label' );
+
+		$formField = $this->newFormFieldFromTags(
+			'Issue',
+			[ $templateField ],
+			[
+				'field',
+				'Status',
+				'mandatory',
+				'hidden',
+				'list',
+				'unique',
+				'input type=checkboxes',
+				'default=Open',
+				'label=Current status',
+				'label msg=pf-status-message',
+				'delimiter=;',
+				'values=Open;Closed;Blocked',
+			]
+		);
+
+		$this->assertInstanceOf( \PFFormField::class, $formField );
+		$this->assertSame( $templateField, $formField->getTemplateField() );
+		$this->assertSame( 'checkboxes', $formField->getInputType() );
+		$this->assertSame( 'Open', $formField->getDefaultValue() );
+		$this->assertSame( 'Current status', $formField->getLabel() );
+		$this->assertSame( 'pf-status-message', $formField->getLabelMsg() );
+		$this->assertTrue( $formField->isMandatory() );
+		$this->assertTrue( $formField->isHidden() );
+		$this->assertTrue( $formField->isList() );
+		$this->assertNull( $formField->isRestricted() );
+		$this->assertFalse( $formField->isDisabled() );
+		$this->assertSame( 'Issue[Status]', $formField->getInputName() );
+		$this->assertSame( [ 'Open', 'Closed', 'Blocked' ], $formField->getPossibleValues() );
+
+		$fieldArgs = $formField->getFieldArgs();
+		$this->assertTrue( $fieldArgs['unique'] );
+		$this->assertSame( ';', $fieldArgs['delimiter'] );
+		$this->assertSame( 'checkboxes', $fieldArgs['input type'] );
+		$this->assertFalse( $formField->getUseDisplayTitle() );
+	}
+
+	/*
+	 *@covers \PFFormField::newFromFormFieldTag
+	 */
+	public function testNewFromFormFieldTagUsesTemplateFieldDefaults() {
+		global $wgPageFormsEmbeddedTemplates;
+
+		$templateField = \PFTemplateField::newFromParams( 'Related page', [
+			'category' => 'Tracked pages',
+			'namespace' => 'Help',
+			'delimiter' => '|',
+			'holds template' => 'Embedded template',
+		] );
+		$templateField->setPossibleValues( [ 'Alpha', 'Beta' ] );
+
+		$formField = $this->newFormFieldFromTags(
+			'Container',
+			[ $templateField ],
+			[ 'field', 'Related page' ]
+		);
+
+		$this->assertSame( $templateField, $formField->getTemplateField() );
+		$this->assertTrue( $formField->isHidden() );
+		$this->assertTrue( $formField->holdsTemplate() );
+		$this->assertTrue( $formField->isList() );
+		$this->assertSame( 'Container[Related page]', $formField->getInputName() );
+		$this->assertSame( [ 'Alpha', 'Beta' ], $formField->getPossibleValues() );
+
+		$fieldArgs = $formField->getFieldArgs();
+		$this->assertSame( 'Tracked pages', $fieldArgs['values from category'] );
+		$this->assertSame( 'Help', $fieldArgs['values from namespace'] );
+		$this->assertSame( '|', $fieldArgs['delimiter'] );
+		$this->assertSame( [ 'Container', 'Related page' ], $wgPageFormsEmbeddedTemplates['Embedded template'] );
+	}
+
+	/*
+	 *@covers \PFFormField::newFromFormFieldTag
+	 */
+	public function testNewFromFormFieldTagMarksMultipleTemplateFields() {
+		$templateField = \PFTemplateField::create( 'Step', null );
+
+		$formField = $this->newFormFieldFromTags(
+			'Workflow',
+			[ $templateField ],
+			[ 'field', 'Step' ],
+			[ 'multiple' ]
+		);
+
+		$this->assertSame( 'Workflow[num][Step]', $formField->getInputName() );
+		$this->assertTrue( $formField->getFieldArg( 'part_of_multiple' ) );
+		$this->assertSame( 'Workflow[Step]', $formField->getFieldArg( 'origName' ) );
+	}
+
+	/*
+	 *@covers \PFFormField::newFromFormFieldTag
+	 */
+	public function testNewFromFormFieldTagSkipsUnknownFieldsDuringStrictParsing() {
+		$knownTemplateField = \PFTemplateField::create( 'Known field', null );
+
+		$formField = $this->newFormFieldFromTags(
+			'Strict template',
+			[ $knownTemplateField ],
+			[ 'field', 'Unknown field', 'mandatory' ],
+			[ 'strict' ]
+		);
+
+		$this->assertInstanceOf( \PFFormField::class, $formField );
+		$this->assertNotSame( $knownTemplateField, $formField->getTemplateField() );
+		$this->assertNull( $formField->getTemplateField()->getFieldName() );
+		$this->assertFalse( $formField->isList() );
+		$this->assertNull( $formField->isMandatory() );
+		$this->assertNull( $formField->getInputName() );
+	}
+
+	/*
+	 *@covers \PFFormField::newFromFormFieldTag
+	 */
+	public function testNewFromFormFieldTagHandlesRestrictionAndDisabledState() {
+		$templateField = \PFTemplateField::create( 'Private note', null );
+		$user = $this->newUserStub( false );
+
+		$restrictedField = $this->newFormFieldFromTags(
+			'Access controlled',
+			[ $templateField ],
+			[ 'field', 'Private note', 'restricted' ],
+			[],
+			false,
+			$user
+		);
+
+		$this->assertTrue( $restrictedField->isRestricted() );
+		$this->assertTrue( $restrictedField->isDisabled() );
+
+		$disabledField = $this->newFormFieldFromTags(
+			'Access controlled',
+			[ $templateField ],
+			[ 'field', 'Private note' ],
+			[],
+			true,
+			$user
+		);
+
+		$this->assertNull( $disabledField->isRestricted() );
+		$this->assertTrue( $disabledField->isDisabled() );
+	}
+
+	/*
+	 *@covers \PFFormField::newFromFormFieldTag
+	 */
+	public function testNewFromFormFieldTagAllowsRestrictedFieldsForPermittedGroups() {
+		$templateField = \PFTemplateField::create( 'Private note', null );
+		$sysop = $this->newUserStub();
+		$userGroupManager = $this->createMock( \MediaWiki\User\UserGroupManager::class );
+		$userGroupManager->method( 'getUserEffectiveGroups' )
+			->with( $sysop )
+			->willReturn( [ 'user', 'sysop' ] );
+		$this->setService( 'UserGroupManager', $userGroupManager );
+
+		$formField = $this->newFormFieldFromTags(
+			'Access controlled',
+			[ $templateField ],
+			[ 'field', 'Private note', 'restricted=sysop' ],
+			[],
+			false,
+			$sysop
+		);
+
+		$this->assertFalse( $formField->isRestricted() );
+		$this->assertFalse( $formField->isDisabled() );
 	}
 
 	public function testCleanupTranslateTags() {
